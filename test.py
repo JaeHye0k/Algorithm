65f5f3859d7571fa0c027918f90d15befc2719b5
leejaehyeok <ahhpc2012@naver.com>
2021-08-16 20:01:31 +0900

    2021-08-16

--- test.py
+++ test.py
@@ -1,12 +1,37 @@
-l = int(input()) # 방학 기간
-a = int(input()) # 국어 총 페이지
-b = int(input()) # 수학 총 페이지
-c = int(input()) # 하루에 풀 수 있는 국어 페이지
-d = int(input()) # 하루에 풀 수 있는 수학 페이지
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
 
-for i in range(l-1,0,-1):
-    a -= c
-    b -= d
-    if a<=0 and b <=0:
-        print(i)
-        break
+n, m = map(int, input().split())
+start = int(input())
+distance = [INF] * (n+1)
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+
+dijkstra(start)
+
+for i in range(1, n+1):
+    if distance[i] == INF:
+        print("INFINITY")
+    else:
+        print(distance[i], end="")
