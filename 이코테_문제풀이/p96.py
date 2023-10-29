13056f31670de91965a748c94a2212228fedd3da
JaeHye0k <ahhpc2012@naver.com>
2023-10-29 18:29:57 +0900

    Using min,max

--- 이코테_문제풀이/p96.py
+++ 이코테_문제풀이/p96.py
@@ -3,17 +3,13 @@
 # 행의 개수 N, 열의 개수 M
 n, m = map(int, input().split())
 data = []
-
+result = 0
 # N * M에 들어갈 데이터 입력
 for i in range(n):
-    data.append(list(map(int, input().split())))
-    # 해당 행의 가장 작은 수가 맨 앞으로 오게 정렬
-    data[i].sort()
-
-result = data[0][0]
-
-for i in range(n):
-    if result < data[i][0]:
-        result = data[i][0]
+    data = list(map(int, input().split()))
+    # 해당 행의 가장 작은 수 찾기
+    min_value = min(data)
+    # 가장 작은 수들 중에 큰 수 찾기
+    result = max(result, min_value)
 
 print(result)
