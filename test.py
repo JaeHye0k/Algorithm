70884d4f3cf9c63da81eb75096622de16ad6eaca
JaeHye0k <ahhpc2012@naver.com>
2023-11-18 19:16:15 +0900

    2839_설탕배달

--- test.py
+++ test.py
@@ -1,37 +1,8 @@
-import heapq
-import sys
-input = sys.stdin.readline
-INF = int(1e9)
+array = [3, 5, 1, 2, 4]  # 5개의 데이터(N=5)
+total = 0           # 합계를 저장할 변수
 
-n, m = map(int, input().split())
-start = int(input())
-distance = [INF] * (n+1)
-graph = [[] for _ in range(n+1)]
+# 모든 데이터를 하나씬 확인하며 연산
+for x in array:
+    total += x
 
-for _ in range(m):
-    a, b, c = map(int, input().split())
-    graph[a].append((b, c))
-
-
-def dijkstra(start):
-    q = []
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
-    while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
-            continue
-        for i in graph[now]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
-
-
-dijkstra(start)
-
-for i in range(1, n+1):
-    if distance[i] == INF:
-        print("INFINITY")
-    else:
-        print(distance[i], end="")
+print(total)  # 결과 출력
