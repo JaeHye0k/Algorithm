1753fbe2f5594a5718f8aa7ca801f7266e6dbe46
JaeHye0k <ahhpc2012@naver.com>
2023-11-18 18:10:42 +0900

    another way

--- 이코테_문제풀이/p99.py
+++ 이코테_문제풀이/p99.py
@@ -1,11 +1,13 @@
 n, k = map(int, input().split())
 count = 0
-while n != 1:
-    if n % k == 0:
-        n = n // k
-        count += 1
-    else:
-        n -= 1
-        count += 1
-count += n % k
+while True:
+    count += n % k  # -1 을 한 횟수
+    n = (n//k)*k  # 1 빼기를 거치고 난 뒤 n의 값
+    if n < k:  # n이 k보다 작을 때(더 이상 나눌 수 없을 때) 반복문 탈출
+        break
+    n //= k  # n을 k로 나눔
+    count += 1  # 나눈 횟수
+
+count += (n-1)
+
 print(count)
