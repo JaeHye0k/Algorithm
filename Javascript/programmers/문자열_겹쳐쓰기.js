bac3d6ceb9566d9b8524913b51d1decd8e6772d7
JaeHye0k <ahhpc2012@naver.com>
2023-12-05 07:39:21 +0900

    Short coding

--- Javascript/programmers/문자열_겹쳐쓰기.js
+++ Javascript/programmers/문자열_겹쳐쓰기.js
@@ -1,9 +1,13 @@
 let result = solution("He11oWor1d", "lloWorl", 2);
 console.log(result);
 
+// function solution(my_string, overwrite_string, s) {
+//   my_string = my_string.split(""); // 문자열 -> 배열
+//   my_string.splice(s, overwrite_string.length, overwrite_string);
+//   my_string = my_string.join(""); // 배열 -> 문자열
+//   return my_string;
+// }
+
 function solution(my_string, overwrite_string, s) {
-  my_string = my_string.split(""); // 문자열 -> 배열
-  my_string.splice(s, overwrite_string.length, overwrite_string);
-  my_string = my_string.join(""); // 배열 -> 문자열
-  return my_string;
+  return my_string.slice(0, s) + overwrite_string + my_string.slice(s + overwrite_string.length);
 }
