b9b568335363b872daf6bf572e22b28e21384148
JaeHye0k <ahhpc2012@naver.com>
2023-12-06 21:52:00 +0900

    Compare using 'Math.max()' and change string to num using 'Number(template literal)'

--- Javascript/programmers/더_크게_합치기.js
+++ Javascript/programmers/더_크게_합치기.js
@@ -2,11 +2,5 @@ console.log(solution(9, 91));
 console.log(solution(89, 8));
 
 function solution(a, b) {
-  a = a.toString();
-  b = b.toString();
-  var a_plus_b = a + b;
-  var b_plus_a = b + a;
-  a_plus_b = parseInt(a_plus_b);
-  b_plus_a = parseInt(b_plus_a);
-  return a_plus_b >= b_plus_a ? a_plus_b : b_plus_a;
+  return Math.max(Number(`${a}${b}`), Number(`${b}${a}`));
 }
