0ba591e677ad34e81939345e040369790b92362b
JaeHye0k <ahhpc2012@naver.com>
2023-12-06 21:07:55 +0900

    Short coding

--- Javascript/programmers/문자열 섞기.js
+++ Javascript/programmers/문자열 섞기.js
@@ -3,11 +3,5 @@ let str2 = "bbbbb";
 console.log(solution(str1, str2));
 
 function solution(str1, str2) {
-  // str1에서 한글자, str2에서 한글자씩 반복문을 사용해서 일일히 추가하기
-  let answer = "";
-  for (let i = 0; i < str1.length; i++) {
-    answer += str1.substring(i, i + 1);
-    answer += str2.substring(i, i + 1);
-  }
-  return answer;
+  return [...str1].map((e, idx) => e + str2[idx]).join(""); // ["ab","ab","ab","ab","ab"] -> "ababababab"
 }
