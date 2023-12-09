ac6e8926e513e9c1871ae405bf540ee95e7852cd
JaeHye0k <ahhpc2012@naver.com>
2023-12-09 22:07:27 +0900

    using map methode and Destructuring assignment

---
+++ Javascript/programmers/Day6/수열과_구간_쿼리_3.js
@@ -0,0 +1,17 @@
+function solution(arr, queries) {
+  queries.map((query) => {
+    [arr[query[0]], arr[query[1]]] = [arr[query[1]], arr[query[0]]];
+  });
+  return arr;
+}
+
+console.log(
+  solution(
+    [0, 1, 2, 3, 4],
+    [
+      [0, 3],
+      [1, 2],
+      [1, 4],
+    ]
+  )
+);
