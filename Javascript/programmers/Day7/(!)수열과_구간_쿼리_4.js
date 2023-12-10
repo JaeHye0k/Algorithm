4f6b86d5e69fce7103b6c476fd74de3afc3efa10
JaeHye0k <ahhpc2012@naver.com>
2023-12-10 10:21:12 +0900

    *실패 코드* forEach 함수 내부에서 'i'는 slice로 자른 배열의 인덱스를 의미하며, 이는 'arr'배열 전체에서의 인덱스와 다를 수 있다.
    해당 코드는 k > e 일 때 잘못된 출력값이 나온다.
    arr = [1,2,3,4,5], queries = [[0,1,4]] 일 경우, arr[0]과 arr[4] 값에 +1을 하여 [2,2,3,4,6]이 출력 되어야 하지만, 해당 코드로 실행할 경우 'i'가 slice(s,e+1)로 인해 잘린 배열의 인덱스 이므로 [1,2]의 인덱스를 가리킨다.이는 i=0~1 이고  arr[0]~arr[1]번 값만 참조할 수 있음을 뜻하며, 따라서 arr[4]의 값에 참조하지 못해 잘못된 출력값인 [2,2,3,4,5]가 출력된다.
    
    또한, 해당 코드는 s > 0일 때 잘못된 출력값이 나온다.
    예를 들어 arr = [1,2,3,4,5], queries = [[1,3,3]] 일 경우 arr[0]과 arr[3]의 값에 +1을 해주어야 한다. 따라서 올바른 출력값으로는 [2,2,3,5,5]가 나와한다. 하지만 해당 코드는 slice(s,e+1)로 인해 s를 0번 인덱스로 하는 [2,3,4]배열이 생기며 'arr'배열과 인덱스가 매핑되지 않는다. 따라서 잘못된 출력값인[2,2,3,4,5]가 출력된다.

---
+++ Javascript/programmers/Day7/(!)수열과_구간_쿼리_4.js
@@ -0,0 +1,11 @@
+function solution(arr, queries) {
+  queries.forEach(([s, e, k]) => {
+    arr.slice(s, e + 1).forEach((item, i) => {
+      i % k === 0 ? (arr[i] += 1) : (arr[i] += 0);
+    });
+  });
+  return arr;
+}
+
+//console.log(solution([1, 2, 3, 4, 5], [[0, 1, 4]]));
+console.log(solution([1, 2, 3, 4, 5], [[1, 3, 3]]));
