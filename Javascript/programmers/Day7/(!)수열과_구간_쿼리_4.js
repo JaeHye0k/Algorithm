3a0170b50c51ace6b3a40b2d3f71164d676e3ffe
JaeHye0k <ahhpc2012@naver.com>
2023-12-10 10:59:27 +0900

    *성공 코드* k > e 이고 s > 0 일 때는 아무것도 바뀌지 않는다. k > e 이고 s = 0 일 때는 arr[0]만 +1된다. (i가 0일 때 어떤 자연수로 나누든 나머지는 0이 나오기 때문)

--- Javascript/programmers/Day7/(!)수열과_구간_쿼리_4.js
+++ Javascript/programmers/Day7/(!)수열과_구간_쿼리_4.js
@@ -1,11 +1,13 @@
 function solution(arr, queries) {
   queries.forEach(([s, e, k]) => {
-    arr.slice(s, e + 1).forEach((item, i) => {
-      i % k === 0 ? (arr[i] += 1) : (arr[i] += 0);
+    arr.forEach((item, i) => {
+      if (i >= s && i <= e) arr[i] += i % k === 0 ? 1 : 0;
     });
   });
   return arr;
 }
 
-console.log(solution([1, 2, 3, 4, 5], [[0, 1, 4]])); // [2, 2, 3, 4, 5]
-console.log(solution([1, 2, 3, 4, 5], [[1, 3, 3]])); // [2, 2, 3, 4, 5]
+console.log(solution([1, 2, 3, 4, 5], [[1, 1, 4]])); // [ 1, 2, 3, 4, 5 ]
+console.log(solution([1, 2, 3, 4, 5], [[1, 3, 3]])); // [ 1, 2, 3, 5, 5 ]
+// prettier-ignore
+console.log(solution([0, 1, 2, 4, 3],[[0, 4, 1],[0, 3, 2],[0, 3, 3],])); // [ 3, 2, 4, 6, 4 ]
