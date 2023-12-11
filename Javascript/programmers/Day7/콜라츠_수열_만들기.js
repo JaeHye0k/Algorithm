f6cebdea2d22022deb61d3100dd3e8ff785f1f7e
JaeHye0k <ahhpc2012@naver.com>
2023-12-11 22:04:18 +0900

    Day7

---
+++ Javascript/programmers/Day7/콜라츠_수열_만들기.js
@@ -0,0 +1,15 @@
+//1. x 가 짝수면 2로 나누고, x 가 홀수면 3*x+1을 한다.          x%2===0? x/2 : 3*x+1
+//2. 1번을 진행해서 나온 x를 배열에 삽입한다.
+//3. 1~2를 x가 1이 나올때까지 계속한다.
+//4. x 가 1이 되면 멈추고 그동안 기록한 x를 배열로 반환한다.      if(x===1) retrun answer
+
+function solution(n) {
+  const answer = [n];
+  while (n !== 1) {
+    n = n % 2 === 0 ? n / 2 : 3 * n + 1;
+    answer.push(n);
+  }
+  return answer;
+}
+
+console.log(solution(10));
