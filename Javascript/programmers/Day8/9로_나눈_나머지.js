0f2b9cb0f661f658698caadcb382055596990195
JaeHye0k <ahhpc2012@naver.com>
2023-12-13 07:29:57 +0900

    Using BigInt object

--- Javascript/programmers/Day8/9로_나눈_나머지.js
+++ Javascript/programmers/Day8/9로_나눈_나머지.js
@@ -1,6 +1,8 @@
+// Number.MAX_SAFE_INTEGER 보다 큰 정수를 처리하는 방법
+// BigInt()
+
 function solution(number) {
-  let answer = [...number].map((e) => Number(e)).reduce((acc, value) => (acc += value));
-  return answer % 9;
+  return BigInt(number) % 9n;
 }
 
 console.log(solution("78720646226947352489"));
