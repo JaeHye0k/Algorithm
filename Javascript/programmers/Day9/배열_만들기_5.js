539cbea94a7dc56b1d8812883f1488e92221db4c
JaeHye0k <ahhpc2012@naver.com>
2023-12-14 22:50:26 +0900

    Day9

--- Javascript/programmers/Day9/배열_만들기_5.js
+++ Javascript/programmers/Day9/배열_만들기_5.js
@@ -6,5 +6,3 @@
 function solution(intStrs, k, s, l) {
   return intStrs.map((e) => +e.slice(s, s + l)).filter((e) => e > k);
 }
-
-console.log(solution(["0123456789", "9876543210", "9999999999999"], 50000, 5, 5, [56789, 99999]));
