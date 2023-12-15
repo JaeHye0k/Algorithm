fb3161194a2c491ba6e82955ae352f00ebab0b71
JaeHye0k <ahhpc2012@naver.com>
2023-12-15 21:49:44 +0900

    short code

--- Javascript/programmers/Day10/세로_읽기.js
+++ Javascript/programmers/Day10/세로_읽기.js
@@ -1,9 +1,4 @@
 function solution(my_string, m, c) {
-  // 문자열을 m개씩 끊어 2차원 배열로 만들어서 행열 구조로 만듦
-  const arr = [];
-  for (var i = 0; i < my_string.length; i += m) {
-    arr.push([...my_string].slice(i, i + m));
-  }
-  // 각 행의 c열에 들어있는 값을 반환한 뒤 문자열로 합치기
-  return arr.map((e) => e[c - 1]).join("");
+  // 굳이 2차원 배열로 만들지 않고 푸는 법
+  return [...my_string].filter((_, i) => i % m === c - 1).join("");
 }
