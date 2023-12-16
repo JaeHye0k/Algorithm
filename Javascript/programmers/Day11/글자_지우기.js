d0f983d11502a17cd6674d5215a60b5e0d6f2556
JaeHye0k <ahhpc2012@naver.com>
2023-12-16 18:40:25 +0900

    short code

--- Javascript/programmers/Day11/글자_지우기.js
+++ Javascript/programmers/Day11/글자_지우기.js
@@ -1,8 +1,8 @@
-// indices에 대응하는 my_string 배열의 인덱스 값을 0으로 변경한 다음 filter로 0이 아닌 요소만 추출하기
+// 1) indices에 대응하는 my_string 배열의 인덱스 값을 0으로 변경한 다음 filter로 0이 아닌 요소만 추출하기
+// 2) my_string을 순회하면서 현재 인덱스가 indices에 포함되어있지 않은 인덱스의 원소들만 추출하기
+
 function solution(my_string, indices) {
-  my_string = [...my_string];
-  indices.forEach((v) => (my_string[v] = 0));
-  return my_string.filter((e) => e !== 0).join("");
+  return [...my_string].filter((_, i) => !indices.includes(i)).join("");
 }
 
 console.log(solution("apporoograpemmemprs", [1, 16, 6, 15, 0, 10, 11, 3]));
