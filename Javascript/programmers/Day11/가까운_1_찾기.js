edacdfd55d9091532028bb13b61b8e8fc2556a52
JaeHye0k <ahhpc2012@naver.com>
2023-12-18 21:50:41 +0900

    Using indexOf

--- Javascript/programmers/Day11/가까운_1_찾기.js
+++ Javascript/programmers/Day11/가까운_1_찾기.js
@@ -1,5 +1,5 @@
 // arr: 0과 1로 이루어진 정수 배열
 // i >= idx && arr[i] === 1 을 충족하는 인덱스 중 가장 작은 인덱스를 반환
 function solution(arr, idx) {
-  return arr.findIndex((v, i) => i >= idx && v === 1);
+  return arr.indexOf(1, idx);
 }
