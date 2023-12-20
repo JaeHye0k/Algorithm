04ad6c4d7cad0f36de3828b3da3138d4ee1207bb
JaeHye0k <ahhpc2012@naver.com>
2023-12-20 22:28:06 +0900

    findLastIndex는 지원하지 않지만 lastIndexOf는 지원함

--- Javascript/programmers/Day12/2의_영역.js
+++ Javascript/programmers/Day12/2의_영역.js
@@ -1,13 +1,10 @@
 // 처음 2를 발견한 인덱스(findIndex) 기억, 마지막으로 2를 발견한 인덱스(findLastIndex) 기억 후 slice로 자르기.
 // 프로그래머스 테스트 환경이 findLastIndex를 지원하지 않음
 function solution(arr) {
-  var last_index = -1;
-  arr.forEach((e, i) => {
-    if (e === 2) last_index = i;
-  });
-  //prettier-ignore
-  var answer = arr.slice(arr.findIndex(e=>e===2), last_index+1);
+  var start = arr.indexOf(2);
+  var end = arr.lastIndexOf(2);
+  var answer = arr.slice(start, end + 1);
   return answer.length ? answer : [-1];
 }
 
-console.log(solution([1, 1, 1]));
+console.log(solution([1, 2, 1]));
