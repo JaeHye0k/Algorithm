a5c9dbda6df8c99112b2ca0e1683446600886bbe
JaeHye0k <ahhpc2012@naver.com>
2024-01-14 13:33:48 +0900

    42min / 20min

--- Python/이코테_문제풀이/p.110_상하좌우.py
+++ Python/이코테_문제풀이/p.110_상하좌우.py
@@ -2,25 +2,25 @@
 # 공간 밖으로 나갈 경우 무시해야 한다.
 # 5
 # R R R U D D
-n = int(input());
-plan = input().split();
+n = int(input())
+plan = input().split()
 
-x, y = 1, 1;
+x, y = 1, 1
 direction = {
     'L':(0,-1),
     'R':(0,1),
     'U':(-1,0),
     'D':(1,0)
-};
+}
 def move(key):
-    global x,y;
-    temp_x = x + direction.get(key)[0];
-    temp_y = y + direction.get(key)[1];
+    global x,y
+    temp_x = x + direction.get(key)[0]
+    temp_y = y + direction.get(key)[1]
     if temp_x > 0 and temp_y > 0 and temp_x <= n and temp_y <= n:
-        x = temp_x;
-        y = temp_y;
+        x = temp_x
+        y = temp_y
 
 for i in plan:
-    move(i);
+    move(i)
 
-print(x,y);
+print(x,y)
--- Python/이코테_문제풀이/p.113_시각.py
+++ Python/이코테_문제풀이/p.113_시각.py
@@ -1,10 +1,10 @@
 # 3중 for문 24*60*60
 
-n = int(input());
-count = 0;
+n = int(input())
+count = 0
 for hour in range(n+1):
     for minute in range(60):
         for second in range(60):
             if '3' in str(hour)+str(minute)+str(second):
-                count+=1;
-print(count);
+                count+=1
+print(count)
