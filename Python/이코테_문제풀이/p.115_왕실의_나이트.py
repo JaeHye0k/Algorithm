f1249dfd7655ffdc2be2a143bd07a7041a406739
JaeHye0k <ahhpc2012@naver.com>
2024-01-14 13:33:48 +0900

    42min / 20min

---
+++ Python/이코테_문제풀이/p.115_왕실의_나이트.py
@@ -0,0 +1,24 @@
+col, row = list(input());
+plans = [
+    (-1,-2),
+    (1,-2),
+    (-2,-1),
+    (-2,1),
+    (-1,2),
+    (1,2),
+    (2,-1),
+    (2,1)
+]
+def move(col, row):
+    global plans;
+    count = 0;
+    row = int(row);
+    col = ord(col)-ord("a")+1;
+    for plan in plans:
+        temp_row = row + plan[0]
+        temp_col = col + plan[1]
+        if(temp_row >= 1 and temp_row <= 8 and temp_col >= 1 and temp_col <= 8):
+            count += 1;
+    return count;
+
+print(move(col,row));
