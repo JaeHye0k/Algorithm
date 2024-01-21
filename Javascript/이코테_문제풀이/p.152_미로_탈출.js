a0158dfd619dde921db80593d56d5b0f8cc98678
JaeHye0k <ahhpc2012@naver.com>
2024-01-21 17:24:01 +0900

    Solved by looking at answer

--- Javascript/이코테_문제풀이/p.152_미로_탈출.js
+++ Javascript/이코테_문제풀이/p.152_미로_탈출.js
@@ -3,23 +3,38 @@ const input = fs.readFileSync("./Javascript/ex.txt").toString().split("\r\n");
 const [n, m] = input[0].split(" ").map(Number);
 const graph = [];
 input.slice(1).forEach((e) => {
-    graph.push(e.split().map(Number));
+    graph.push(e.split("").map(Number));
 });
 const queue = [];
 
+const move = [
+    [-1, 0], // up
+    [1, 0], // down
+    [0, -1], // left
+    [0, 1], // right
+];
+
 function bfs(x, y) {
     queue.push([x, y]);
-    graph[x][y] = 0;
     while (queue.length > 0) {
-        let v = queue.shift();
-        let x,
-            y = [...v];
-        // 범위를 초과할 경우
-        if (x < 0 || x >= n || y < 0 || y >= m) continue;
-
-        queue.push([x + 1, y]);
+        [x, y] = [...queue.shift()];
+        // 상,하,좌,우 위치 확인
+        for (let d of move) {
+            let nx = x + d[0];
+            let ny = y + d[1];
+            // 범위를 벗어날 경우 무시
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            // 괴물이 있는 부분일 경우 무시
+            if (graph[nx][ny] === 0) continue;
+            // 방문한 적 없는 노드일 경우 최단 거리 기록
+            if (graph[nx][ny] === 1) {
+                graph[nx][ny] = graph[x][y] + 1;
+                queue.push([nx, ny]);
+            }
+        }
     }
+    // 위 과정이 수행되고 나면 graph[n-1][m-1]에 최단 거리가 기록됨
+    return graph[n - 1][m - 1];
 }
 
-let result = 1;
-bfs(0, 0);
+console.log(bfs(0, 0));
