a0500ea10e65351f57c6a74f25d983d833fba9e7
JaeHye0k <ahhpc2012@naver.com>
2024-01-21 21:40:09 +0900

    Solve (\r\n -> \n)

--- Javascript/beakjoon/1012_유기농_배추.js
+++ Javascript/beakjoon/1012_유기농_배추.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
-const input = fs.readFileSync("./Javascript/ex.txt").toString().split("\r\n");
+const input = fs.readFileSync("./Javascript/ex.txt").toString().split("\n");
 const t = Number(input.shift());
+
 function test() {
     const [m, n, k] = [...input.shift().split(" ").map(Number)];
     const graph = Array.from(Array(n), () => Array(m).fill(0));
@@ -13,25 +14,26 @@ function test() {
 
     function dfs(x, y) {
         // 범위를 벗어날 경우
-        if (x < 0 || y < 0 || x >= n || y >= m) return;
+        if (x < 0 || y < 0 || x >= n || y >= m) return false;
         // 방문한 적 있는 노드이거나 경우 배추가 없는 위치일 경우
-        if (graph[x][y] === 0) return;
-        // 방문 처리
-        graph[x][y] = 0;
-        dfs(x - 1, y);
-        dfs(x + 1, y);
-        dfs(x, y - 1);
-        dfs(x, y + 1);
+        if (graph[x][y] === 1) {
+            // 방문 처리
+            graph[x][y] = 0;
+            dfs(x - 1, y);
+            dfs(x + 1, y);
+            dfs(x, y - 1);
+            dfs(x, y + 1);
+            return true;
+        }
+        return false;
     }
 
     function solution() {
         let result = 0;
         for (let i = 0; i < n; i++) {
             for (let j = 0; j < m; j++) {
-                // 배추가 있는 곳일 경우
-                if (graph[i][j] === 1) {
+                if (dfs(i, j) === true) {
                     result++;
-                    dfs(i, j);
                 }
             }
         }
