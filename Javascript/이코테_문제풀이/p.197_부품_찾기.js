3b5f4af8fa661e50dab403b807d63d0b58357c01
JaeHye0k <ahhpc2012@naver.com>
2024-01-23 22:07:11 +0900

    Solve (39m 28s)

---
+++ Javascript/이코테_문제풀이/p.197_부품_찾기.js
@@ -0,0 +1,28 @@
+const { stdout } = require("process");
+const fs = require("fs");
+const input = fs.readFileSync("./Javascript/ex.txt").toString().trim().split("\n");
+const n = Number(input[0]);
+const n_arr = input[1].split(" ").map(Number);
+const m = Number(input[2]);
+const m_arr = input[3].split(" ").map(Number);
+
+n_arr.sort();
+
+for (let i = 0; i < m; i++) {
+    let start = 0;
+    let end = n;
+    let mid;
+    let target = m_arr[i];
+    let result = false;
+    while (start <= end) {
+        mid = Math.floor((start + end) / 2);
+        if (n_arr[mid] === target) {
+            result = true;
+            break;
+        } else if (n_arr[mid] > target) end = mid - 1;
+        else start = mid + 1;
+    }
+    if (result) process.stdout.write("yes ");
+    else process.stdout.write("no ");
+}
+console.log();
