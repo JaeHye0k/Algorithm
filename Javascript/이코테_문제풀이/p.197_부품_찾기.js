4ca0eb45d45b09105209985462549eb6fc3554a3
JaeHye0k <ahhpc2012@naver.com>
2024-01-24 06:48:52 +0900

    refactoring

--- Javascript/이코테_문제풀이/p.197_부품_찾기.js
+++ Javascript/이코테_문제풀이/p.197_부품_찾기.js
@@ -5,23 +5,20 @@ const n = Number(input[0]);
 const n_arr = input[1].split(" ").map(Number);
 const m = Number(input[2]);
 const m_arr = input[3].split(" ").map(Number);
-
 n_arr.sort();
 
-for (let i = 0; i < m; i++) {
-    let start = 0;
-    let end = n - 1;
-    let mid;
-    let target = m_arr[i];
-    let result = false;
+function binary_search(array, target, start, end) {
     while (start <= end) {
         mid = Math.floor((start + end) / 2);
-        if (n_arr[mid] === target) {
-            result = true;
-            break;
-        } else if (n_arr[mid] > target) end = mid - 1;
+        if (array[mid] === target) return mid;
+        else if (array[mid] > target) end = mid - 1;
         else start = mid + 1;
     }
+    return;
+}
+
+for (let i = 0; i < m; i++) {
+    let result = binary_search(n_arr, m_arr[i], 0, n - 1);
     if (result) process.stdout.write("yes ");
     else process.stdout.write("no ");
 }
