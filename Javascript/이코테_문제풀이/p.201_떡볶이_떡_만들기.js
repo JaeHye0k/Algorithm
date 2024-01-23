5851e3798b352d9e16263a216e405808c65038ca
JaeHye0k <ahhpc2012@naver.com>
2024-01-24 07:42:50 +0900

    Unsolved

---
+++ Javascript/이코테_문제풀이/p.201_떡볶이_떡_만들기.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync("./Javascript/ex.txt").toString().trim().split("\n");
+const [n, m] = input[0].split(" ").map(Number);
+const array = input[1].split(" ").map(Number);
+const result = [];
+
+function binary_search(array, m, start, end) {
+    while (start <= end) {
+        let sum = 0;
+        let mid = Math.floor((start + end) / 2);
+        for (let i = 0; i < n; i++) {
+            sum += array[i] - mid;
+        }
+        if (sum >= m) {
+            result.push(sum);
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+}
+binary_search(array, m, 0, Math.max(...array));
+console.log(Math.min(...result));
