d933692a0e4c565b5fb08e10ac92c3315b15324b
JaeHye0k <ahhpc2012@naver.com>
2024-01-31 20:34:25 +0900

    맞았습니다

--- Javascript/beakjoon/자료구조/BOJ1620_나는야 포켓몬 마스터 이다솜.js
+++ Javascript/beakjoon/자료구조/BOJ1620_나는야 포켓몬 마스터 이다솜.js
@@ -4,24 +4,21 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
-function solution(obj, strNum) {
-    strNum.forEach((e) => {
-        let result = Object.keys(obj).find((key) => obj[key] === e) || obj[e];
-        console.log(result);
-    });
-}
-
 let input = [];
 rl.on("line", (line) => {
     input.push(line);
 }).on("close", () => {
     const [n, m] = input.shift().split(" ").map(Number);
-    const poke = input.filter((e, i) => i < n);
-    const obj = {};
-    poke.forEach((e, i) => {
-        obj[i + 1] = e;
+    const poke = input.splice(0, n);
+    const numToStr = new Map();
+    const strToNum = new Map();
+    for (let i = 0; i < n; i++) {
+        numToStr.set(i + 1, poke[i]);
+        strToNum.set(poke[i], i + 1);
+    }
+    input.forEach((e) => {
+        if (isNaN(e)) console.log(strToNum.get(e));
+        else console.log(numToStr.get(+e));
     });
-    const strNum = input.filter((e, i) => i >= n);
-    solution(obj, strNum);
     process.exit();
 });
