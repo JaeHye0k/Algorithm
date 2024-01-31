d0f4bfed46892c48e5cfc478f2b9e145e7af8d01
JaeHye0k <ahhpc2012@naver.com>
2024-01-31 21:53:19 +0900

    맞았습니다

--- Javascript/beakjoon/자료구조/BOJ2164_카드2.js
+++ Javascript/beakjoon/자료구조/BOJ2164_카드2.js
@@ -1,3 +1,39 @@
+class Queue {
+    constructor() {
+        this.storage = {}; // 값을 저장할 객체
+        this.front = 0; // 첫 번째 원소를 가리킬 포인터
+        this.rear = 0; // 마지막 원소를 가리킬 포인터
+    }
+    size() {
+        // rear가 가리키는 곳이 비어있을 때가 아무 데이터가 없는 경우다.
+        if (this.storage[this.rear] === undefined) return 0;
+        else return this.rear - this.front + 1;
+    }
+    enqueue(value) {
+        // 큐가 비어있을 경우
+        if (this.size() === 0) {
+            // 0번 위치에 값을 넣어 놓는다.
+            // 포인터는 데이터를 추출했을 때 큐가 비어있으면 그때 0으로 수정해줄 것이다.
+            this.storage["0"] = value;
+        } else {
+            this.rear += 1;
+            this.storage[this.rear] = value;
+        }
+    }
+    dequeue() {
+        let temp = this.storage[this.front];
+        delete this.storage[this.front];
+
+        if (this.front === this.rear) {
+            this.front = 0;
+            this.rear = 0;
+        } else {
+            this.front += 1;
+        }
+        return temp;
+    }
+}
+
 const readline = require("readline");
 const rl = readline.createInterface({
     input: process.stdin,
@@ -10,16 +46,17 @@ rl.on("line", (line) => {
     input.push(line);
 }).on("close", () => {
     const n = +input[0];
-    let arr = [];
-    for (let i = 0; i < n; i++) {
-        arr[i] = i + 1;
+    let queue = new Queue();
+    for (let i = 1; i <= n; i++) {
+        queue.enqueue(i);
     }
-    while (arr.length > 1) {
-        arr.shift();
-        if (arr.length <= 1) break;
-        let v = arr.shift();
-        arr.push(v);
+    let v = 0;
+    while (queue.size() > 1) {
+        queue.dequeue();
+        if (queue.size() <= 1) break;
+        v = queue.dequeue();
+        queue.enqueue(v);
     }
-    console.log(arr[0]);
+    console.log(queue.dequeue());
     process.exit();
 });
