2cb32ed77de664039988a68e3c23cac9ed9508ef
JaeHye0k <ahhpc2012@naver.com>
2024-02-05 06:57:40 +0900

    unsolved

--- Javascript/programmers/고득점kit/가장_큰_수.js
+++ Javascript/programmers/고득점kit/가장_큰_수.js
@@ -1,14 +1,8 @@
 function solution(numbers) {
-    let copyNums = numbers
-        .map((num, i) => {
-            return [i, Number(num.toString().repeat(3))];
-        })
-        .sort((a, b) => {
-            if (b[1] - a[1] === 0) return b[1] - a[1];
-        });
-    let answer = "";
-    copyNums.forEach(([i, num]) => {
-        answer += numbers[i].toString();
-    });
-    return Number(answer).toString();
+    let answer = numbers
+        .map(String)
+        .sort((a, b) => b + a - (a + b))
+        .join("");
+    return String(BigInt(answer));
+    //return String(Number(answer)) => 실패
 }
