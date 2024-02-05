be4ba3b25a4596392e2e32df6ed83b9d708ff5be
JaeHye0k <ahhpc2012@naver.com>
2024-02-06 07:26:59 +0900

    solve

--- Javascript/programmers/고득점kit/완주 하지 못한 선수.js
+++ Javascript/programmers/고득점kit/완주 하지 못한 선수.js
@@ -1,13 +1,29 @@
+// function solution(participant, completion) {
+//     let map1 = new Map();
+//     participant.forEach(e=>{
+//         map1.set(e,map1.get(e)+1||1);
+//     })
+//     completion.forEach(e=>{
+//         map1.set(e,map1.get(e)-1);
+//     })
+//     let answer = [...map1.entries()].filter(([key,value])=>value !== 0);
+//     answer = answer[0][0];
+//     return answer;
+// }
+
 function solution(participant, completion) {
-    let map1 = new Map();
-    participant.forEach(e=>{
-        if(map1.has(e)) map1.set(e, map1.get(e)+1);
-        else map1.set(e, 1);
-    })
-    completion.forEach(e=>{
-        map1.set(e,map1.get(e)-1);
-    })
-    let answer = [...map1.entries()].filter(([key,value])=>value !== 0);
-    answer = answer[0][0];
+    let obj = {};
+    let answer = "";
+    participant.forEach((e) => {
+        obj[e] = obj[e] + 1 || 1;
+    });
+    completion.forEach((e) => {
+        obj[e] = obj[e] - 1;
+    });
+    for (let e in obj) {
+        if (obj[e] === 1) answer = e;
+    }
     return answer;
 }
+
+// 정확성: 58.3, 호율성: 41.7
