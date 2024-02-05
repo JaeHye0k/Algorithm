258963f8df32166754faf1bc5ff70cf42abdf6e3
JaeHye0k <ahhpc2012@naver.com>
2024-02-05 22:42:08 +0900

    unsolved (clean code)

--- Javascript/programmers/고득점kit/모의고사.js
+++ Javascript/programmers/고득점kit/모의고사.js
@@ -2,16 +2,17 @@ function solution(answers) {
     let first = [1, 2, 3, 4, 5];
     let seconde = [2, 1, 2, 3, 2, 4, 2, 5];
     let third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-    let count = [0, 0, 0];
-    answers.forEach((e, i) => {
-        if (e === first[i % first.length]) count[0]++;
-        if (e === seconde[i % seconde.length]) count[1]++;
-        if (e === third[i % third.length]) count[2]++;
-    });
+
+    let count1 = answers.filter((e, i) => e === first[i % first.length]).length;
+    let count2 = answers.filter((e, i) => e === seconde[i % seconde.length]).length;
+    let count3 = answers.filter((e, i) => e === third[i % third.length]).length;
+
+    let maxValue = Math.max(count1, count2, count3);
+
     let answer = [];
-    let maxValue = Math.max(...count);
-    count.forEach((e, i) => {
-        if (maxValue === e) answer.push(i + 1);
-    });
-    return answer;
+    if (count1 === maxValue) answer.push(1);
+    if (count2 === maxValue) answer.push(2);
+    if (count3 === maxValue) answer.push(3);
+
+    return answer.sort((a, b) => a - b);
 }
