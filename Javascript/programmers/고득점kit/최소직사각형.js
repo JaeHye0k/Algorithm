5c2d226adecf7acd477f1a1d5c4c1e4b0133d4b9
JaeHye0k <ahhpc2012@naver.com>
2024-02-05 21:30:59 +0900

    solve

--- Javascript/programmers/고득점kit/최소직사각형.js
+++ Javascript/programmers/고득점kit/최소직사각형.js
@@ -4,10 +4,10 @@ function solution(sizes) {
     sizes.forEach(([w, h]) => {
         let temp1 = Math.abs(width - w) + Math.abs(height - h);
         let temp2 = Math.abs(width - h) + Math.abs(height - w);
-        if (temp1 < temp2) {
+        if (temp1 <= temp2) {
             width = w > width ? w : width;
             height = h > height ? h : height;
-        } else if (temp1 > temp2) {
+        } else {
             width = h > width ? h : width;
             height = w > height ? w : height;
         }
