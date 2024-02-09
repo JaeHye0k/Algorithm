0aa859ab0b4830669fcea313bcfb3731214af064
JaeHye0k <ahhpc2012@naver.com>
2024-02-09 14:28:48 +0900

    BFS

--- Javascript/programmers/고득점kit/타겟넘버.js
+++ Javascript/programmers/고득점kit/타겟넘버.js
@@ -1,53 +1,72 @@
-// 통과 (BFS)
-class Queue {
-    constructor() {
-        this.storage = {};
-        this.front = 0;
-        this.rear = 0;
-    }
-    size() {
-        // 큐가 비어있을 경우
-        if (this.storage[this.rear] === undefined) return 0;
-        return this.rear - this.front + 1;
-    }
-    dequeue() {
-        let temp = this.storage[this.front];
-        delete this.storage[this.front];
-
-        if (this.front === this.rear) {
-            this.front = 0;
-            this.rear = 0;
-        } else {
-            this.front++;
-        }
-        return temp;
-    }
-    enqueue(value) {
-        if (this.size() === 0) {
-            this.storage["0"] = value;
-        } else {
-            this.rear++;
-            this.storage[this.rear] = value;
-        }
-    }
-}
-
-// BFS
+// 통과 (BFS) 조금 더 간결한 버전
 function solution(numbers, target) {
     let n = numbers.length;
-    let queue = new Queue();
-    queue.enqueue([0, 0]);
+    let queue = [[0, 0]];
+    let front = 0;
     let answer = 0;
-    while (queue.size()) {
-        let [v, depth] = queue.dequeue();
+    while (queue.length) {
+        if (front >= queue.length) break; // front가 범위를 벗어나면 중단하기
+        let [v, depth] = queue[front];
+        front++;
         if (depth < n) {
-            queue.enqueue([v + numbers[depth], depth + 1]);
-            queue.enqueue([v - numbers[depth], depth + 1]);
+            queue.push([v + numbers[depth], depth + 1]);
+            queue.push([v - numbers[depth], depth + 1]);
         }
         if (depth === n && v === target) answer++;
     }
     return answer;
 }
+
+// 통과 (BFS)
+// class Queue {
+//     constructor() {
+//         this.storage = {};
+//         this.front = 0;
+//         this.rear = 0;
+//     }
+//     size() {
+//         // 큐가 비어있을 경우
+//         if (this.storage[this.rear] === undefined) return 0;
+//         return this.rear - this.front + 1;
+//     }
+//     dequeue() {
+//         let temp = this.storage[this.front];
+//         delete this.storage[this.front];
+
+//         if (this.front === this.rear) {
+//             this.front = 0;
+//             this.rear = 0;
+//         } else {
+//             this.front++;
+//         }
+//         return temp;
+//     }
+//     enqueue(value) {
+//         if (this.size() === 0) {
+//             this.storage["0"] = value;
+//         } else {
+//             this.rear++;
+//             this.storage[this.rear] = value;
+//         }
+//     }
+// }
+
+// // BFS
+// function solution(numbers, target) {
+//     let n = numbers.length;
+//     let queue = new Queue();
+//     queue.enqueue([0, 0]);
+//     let answer = 0;
+//     while (queue.size()) {
+//         let [v, depth] = queue.dequeue();
+//         if (depth < n) {
+//             queue.enqueue([v + numbers[depth], depth + 1]);
+//             queue.enqueue([v - numbers[depth], depth + 1]);
+//         }
+//         if (depth === n && v === target) answer++;
+//     }
+//     return answer;
+// }
 
 // 통과 (DFS)
 // function solution(numbers, target) {
