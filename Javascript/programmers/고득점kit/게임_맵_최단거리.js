cdd3845bc98a400eba9928e96f826eeb042804ab
JaeHye0k <ahhpc2012@naver.com>
2024-02-09 17:30:47 +0900

    복습 필요

--- Javascript/programmers/고득점kit/게임_맵_최단거리.js
+++ Javascript/programmers/고득점kit/게임_맵_최단거리.js
@@ -31,37 +31,28 @@ class Queue {
 }
 
 function solution(maps) {
-    let n = maps[0].length - 1;
-    let m = maps.length - 1;
-    let direction = [
-        [0, -1],
-        [0, 1],
-        [-1, 0],
-        [1, 0],
-    ];
+    let xLen = maps[0].length;
+    let yLen = maps.length;
+    let xGoal = xLen - 1;
+    let yGoal = yLen - 1;
+    let dx = [0, 0, -1, 1];
+    let dy = [-1, 1, 0, 0];
+
     let queue = new Queue();
-    queue.enqueue([0, 0]); // 좌표값 저장
-    maps[0][0] = 0;
+    queue.enqueue([0, 0, 1]);
+
     while (queue.size()) {
-        let [x, y] = queue.dequeue(); // 현재 좌표값
-        for (let [dx, dy] of direction) {
-            let nx = x + dx;
-            let ny = y + dy;
-            if (nx < 0 || ny < 0 || nx > n || ny > m) continue; // 범위를 벗어날 경우
-            if (maps[ny][nx] === 1) {
-                queue.enqueue([x, y]);
-                maps[ny][nx] = maps[y][x] + 1;
+        let [x, y, count] = queue.dequeue();
+        if (x === xGoal && y === yGoal) return count;
+
+        for (let i = 0; i < 4; i++) {
+            let nx = x + dx[i];
+            let ny = y + dy[i];
+            if (nx >= 0 && ny >= 0 && nx < xLen && ny < yLen && maps[ny][nx] === 1) {
+                queue.enqueue([nx, ny, count + 1]);
+                maps[ny][nx] = 0;
             }
         }
     }
-    return maps[n][m] === 1 ? -1 : maps[n][m] + 1;
+    return -1;
 }
-
-let answer = solution([
-    [1, 0, 1, 1, 1],
-    [1, 0, 1, 0, 1],
-    [1, 0, 1, 1, 1],
-    [1, 1, 1, 0, 1],
-    [0, 0, 0, 0, 1],
-]);
-console.log(answer);
