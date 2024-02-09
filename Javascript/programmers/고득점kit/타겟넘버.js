a77b355f542fc79ff80881a7ba595f240372e8a4
JaeHye0k <ahhpc2012@naver.com>
2024-02-09 14:08:22 +0900

    BFS -> 통과

--- Javascript/programmers/고득점kit/타겟넘버.js
+++ Javascript/programmers/고득점kit/타겟넘버.js
@@ -1,35 +1,70 @@
-// 테스트 케이스 1,2 시간초과 (BFS)
-// function solution(numbers, target) {
-//     let n = numbers.length;
-//     let queue = [[0, 0]];
-//     let answer = 0;
-//     while (queue.length) {
-//         let [v, depth] = queue.shift();
-//         if (depth < n) {
-//             queue.push([v + numbers[depth], depth + 1]);
-//             queue.push([v - numbers[depth], depth + 1]);
-//         }
-//         if (depth === n && v === target) answer++;
-//     }
-//     return answer;
-// }
+// 통과 (BFS)
+class Queue {
+    constructor() {
+        this.storage = {};
+        this.front = 0;
+        this.rear = 0;
+    }
+    size() {
+        // 큐가 비어있을 경우
+        if (this.storage[this.rear] === undefined) return 0;
+        return this.rear - this.front + 1;
+    }
+    dequeue() {
+        let temp = this.storage[this.front];
+        delete this.storage[this.front];
 
-// 통과 (DFS)
-function solution(numbers, target) {
-    let n = numbers.length;
-    let answer = 0;
-    function dfs(sum, depth) {
-        if (depth < n) {
-            dfs(sum + numbers[depth], depth + 1);
-            dfs(sum - numbers[depth], depth + 1);
+        if (this.front === this.rear) {
+            this.front = 0;
+            this.rear = 0;
         } else {
-            if (sum === target) answer++;
-            return;
+            this.front++;
+        }
+        return temp;
+    }
+    enqueue(value) {
+        if (this.size() === 0) {
+            this.storage["0"] = value;
+        } else {
+            this.rear++;
+            this.storage[this.rear] = value;
         }
     }
-    dfs(0, 0);
+}
+
+// BFS
+function solution(numbers, target) {
+    let n = numbers.length;
+    let queue = new Queue();
+    queue.enqueue([0, 0]);
+    let answer = 0;
+    while (queue.size()) {
+        let [v, depth] = queue.dequeue();
+        if (depth < n) {
+            queue.enqueue([v + numbers[depth], depth + 1]);
+            queue.enqueue([v - numbers[depth], depth + 1]);
+        }
+        if (depth === n && v === target) answer++;
+    }
     return answer;
 }
+
+// 통과 (DFS)
+// function solution(numbers, target) {
+//     let n = numbers.length;
+//     let answer = 0;
+//     function dfs(sum, depth) {
+//         if (depth < n) {
+//             dfs(sum + numbers[depth], depth + 1);
+//             dfs(sum - numbers[depth], depth + 1);
+//         } else {
+//             if (sum === target) answer++;
+//             return;
+//         }
+//     }
+//     dfs(0, 0);
+//     return answer;
+// }
 console.time();
 let answer = solution(
     Array.from({ length: 20 }, () => 1),
