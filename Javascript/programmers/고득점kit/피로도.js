1943ea288cc18e88799dcce438b334e94a5ba7c9
JaeHye0k <ahhpc2012@naver.com>
2024-02-10 22:21:53 +0900

    복습 필요 (1/3) (23분)

--- Javascript/programmers/고득점kit/피로도.js
+++ Javascript/programmers/고득점kit/피로도.js
@@ -1,42 +1,19 @@
-// bfs
+// dfs
 function solution(k, dungeons) {
-    let answer = 0;
-    let queue = [[k, []]];
-
-    while (queue.length) {
-        let [k, route] = queue.shift();
-        for (let i = 0; i < dungeons.length; i++) {
+    let n = dungeons.length;
+    let visited = Array.from({ length: n }, () => false);
+    let answer = [];
+    function dfs(tired, count) {
+        answer.push(count);
+        for (let i = 0; i < n; i++) {
             let [a, b] = dungeons[i];
-            if (k >= a && !route.includes(i)) {
-                queue.push([k - b, route.concat(i)]);
+            if (tired >= a && !visited[i]) {
+                visited[i] = true;
+                dfs(tired - b, count + 1);
+                visited[i] = false;
             }
-            answer = Math.max(answer, route.length);
         }
     }
-    return answer;
+    dfs(k, 0);
+    return Math.max(...answer);
 }
-// dfs
-// function solution(k, dungeons) {
-//     let n = dungeons.length;
-//     let visited = Array(n).fill(false);
-//     let answer = 0;
-//     function dfs(k, count){
-//         answer = Math.max(answer, count);
-//         for(let i=0; i<n; i++){
-//             let [a,b] = dungeons[i];
-//             if(k >= a && !visited[i]){
-//                 visited[i] = true;
-//                 dfs(k-b, count+1);
-//                 visited[i] = false;
-//             }
-//         }
-//         return answer;
-//     }
-//     dfs(k,0);
-//     return answer;
-// }
-solution(80, [
-    [80, 20],
-    [50, 40],
-    [30, 10],
-]);
