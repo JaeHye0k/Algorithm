c403f787fb4cd175a798bf70b37ff6bd4f63673a
JaeHye0k <ahhpc2012@naver.com>
2024-02-10 09:36:31 +0900

    혼자 해결 (1시간)

--- Javascript/programmers/고득점kit/단어_변환.js
+++ Javascript/programmers/고득점kit/단어_변환.js
@@ -30,21 +30,23 @@ class Queue {
     }
 }
 function solution(begin, target, words) {
-    if (!words.some((e) => e === target)) return 0; // words에 target 단어가 없을 경우
+    // words에 target이 없을 경우 0 반환
+    if (!words.some((e) => e === target)) return 0;
 
     const n = words.length; // words의 길이
     const wLen = begin.length; // 단어 하나의 길이
     let answer = 0;
-
     const visited = Array.from({ length: n }, () => false);
+
     const queue = new Queue();
-    queue.enqueue([begin, 1]);
+    queue.enqueue([begin, 0]); // [현재 단어,현재 단계]
     while (queue.size()) {
         let [cur, count] = queue.dequeue();
-        if (cur === target) break; // 현재 단어가 target과 일치하다면 반복문 중단
+        // 현재 단어가 target과 일치하다면 반복문 중단
+        if (cur === target) break;
         words.forEach((word, i) => {
+            // 방문하지 않은 단어라면
             if (!visited[i]) {
-                // 방문하지 않은 단어라면
                 let splitCur = cur.split("");
                 let splitWord = word.split("");
                 let differ = 0;
@@ -52,14 +54,14 @@ function solution(begin, target, words) {
                 for (let i = 0; i < wLen; i++) {
                     if (splitCur[i] !== splitWord[i]) differ++;
                 }
+                // 한 글자만 다를 경우 큐에 삽입 후 방문 처리
                 if (differ === 1) {
-                    // 하나만 다를 경우
-                    queue.enqueue([word, count + 1]); // count++로 해줄경우 증가 안됨
+                    queue.enqueue([word, count + 1]); // count+1 대신 count++로 할 경우 증가 안됨
                     visited[i] = true;
                 }
             }
         });
         answer = count;
     }
-    return answer;
+    return answer + 1;
 }
