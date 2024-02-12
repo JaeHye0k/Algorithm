43025409e71b7f35790da98b29c680434a9901c9
JaeHye0k <ahhpc2012@naver.com>
2024-02-12 13:02:11 +0900

    통과 (1시간 25분)

--- Javascript/beakjoon/완전탐색/BOJ2309_일곱 난쟁이.js
+++ Javascript/beakjoon/완전탐색/BOJ2309_일곱 난쟁이.js
@@ -1,12 +1,25 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./Javascript/input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n").map(Number);
+const { stdout } = require("process");
 
-let input = [];
-rl.on("line", (line) => {
-    input.push(line);
-}).on("close", () => {
-    process.exit();
-});
+let total = input.reduce((acc, cur) => (acc += cur));
+let index = [];
+
+for (let i = 0; i < 9; i++) {
+    let temp = total - input[i];
+    for (let j = 0; j < 9; j++) {
+        if (j !== i && temp - input[j] === 100) {
+            index.push(i);
+            index.push(j);
+            break;
+        }
+    }
+}
+
+index = index.splice(0, 2); // 가능한 정답이 여러 가지인 경우 고려
+let answer = input.filter((e, i) => !index.includes(i));
+answer.sort((a, b) => a - b);
+for (let dwarf of answer) {
+    process.stdout.write(dwarf + "\n");
+}
