22724333dab5337e559df7eef3950a28fd983095
JaeHye0k <ahhpc2012@naver.com>
2024-02-13 23:27:18 +0900

    복습 필요 ( 0 / 5)

--- Javascript/beakjoon/BOJ22868_산책(small).js
+++ Javascript/beakjoon/BOJ22868_산책(small).js
@@ -3,20 +3,21 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./Javascript/inp
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 const [N, M] = input[0].split(" ").map(Number);
 const [S, E] = input[M + 1].split(" ").map(Number);
-const line = input.splice(1, M).map((e) => e.split(" ").map(Number));
-const graph = [[]];
-const visited = Array.from({ length: N + 1 }, () => false);
-let answer = 0;
+const lines = input.splice(1, M).map((e) => e.split(" ").map(Number));
+const graph = Array.from({ length: N + 1 }, () => new Array());
+const visited = Array.from({ length: N + 1 }, () => [false, []]);
 
-line.forEach(([s, e]) => {
-    if (graph[s] === undefined) graph[s] = [e];
-    else graph[s].push(e);
-    if (graph[e] === undefined) graph[e] = [s];
-    else graph[e].push(s);
+// 그래프 형태로 만들기
+lines.forEach(([s, e]) => {
+    graph[s].push(e);
+    graph[e].push(s);
 });
-graph.forEach((e, i) => {
+
+// 작은 수부터 방문해주기 위해 오름차순으로 정렬
+graph.forEach((_, i) => {
     graph[i].sort((a, b) => a - b);
 });
+
 class Queue {
     constructor() {
         this.storage = {};
@@ -49,43 +50,48 @@ class Queue {
     }
 }
 
-let queue = new Queue();
-queue.enqueue([S, [S]]);
-visited[S] = true;
-let newPath = [];
+// visited[n][0] : n번 정점의 방문 여부
+// visited[n][1] : n번 정점까지의 경로
 
+let queue = new Queue();
+queue.enqueue(S);
+visited[S][0] = true;
+visited[S][1] = [S];
 while (queue.size()) {
-    let [v, path] = queue.dequeue();
+    let v = queue.dequeue();
     if (v === E) break;
     graph[v].forEach((e) => {
-        if (!visited[e]) {
-            visited[e] = true;
-            newPath = [...path];
-            newPath.push(e);
-            queue.enqueue([e, newPath]);
+        if (!visited[e][0]) {
+            visited[e][0] = true;
+            visited[e][1] = [...visited[v][1], e];
+            queue.enqueue(e);
         }
     });
 }
-// 왔던 경로만 제외하고 방문 해제하기
-visited.fill(false);
-newPath.forEach((e) => {
-    visited[e] = true;
-});
-// 시작노드로 가야하니 시작 노드도 방문 해제
-visited[S] = false;
 
+// E까지 온 경로에 포함되지 않았다면 방문 해제해주고 경로 초기화
+for (let i = 1; i <= N; i++) {
+    if (!visited[E][1].includes(i)) {
+        visited[i][0] = false;
+        visited[i][1] = [];
+    }
+}
+
+// 큐 초기화
 queue = new Queue();
-queue.enqueue([E, newPath]);
+queue.enqueue(E);
+visited[S][0] = false; // 시작노드로 가야하니 시작 노드도 방문 해제
+visited[S][1] = [];
 while (queue.size()) {
-    let [v, path] = queue.dequeue();
+    let v = queue.dequeue();
     if (v === S) break;
     graph[v].forEach((e) => {
-        if (!visited[e]) {
-            visited[e] = true;
-            newPath = [...path];
-            newPath.push(e);
-            queue.enqueue([e, newPath]);
+        if (!visited[e][0]) {
+            visited[e][0] = true;
+            visited[e][1] = [...visited[v][1], e];
+            queue.enqueue(e);
         }
     });
 }
-console.log(newPath.length - 1);
+
+console.log(visited[S][1].length - 1);
