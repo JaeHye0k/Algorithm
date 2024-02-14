cc587b52162b6c58ebba949f457fd224e1659bd5
JaeHye0k <ahhpc2012@naver.com>
2024-02-14 16:45:56 +0900

    스스로 해결 (39분)

--- Javascript/beakjoon/구현/BOJ2979_트럭주차.js
+++ Javascript/beakjoon/구현/BOJ2979_트럭주차.js
@@ -3,7 +3,17 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./Javascript/inp
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 const [A, B, C] = input[0].split(" ").map(Number);
 const time = input.slice(1).map((e) => e.split(" ").map(Number));
+
 let answer = 0;
-time.sort((a, b) => a[0] - b[0]);
-time.forEach();
-//14분
+let overlap;
+
+for (let i = 1; i <= 100; i++) {
+    overlap = 0;
+    if (time[0][0] <= i && i < time[0][1]) overlap++;
+    if (time[1][0] <= i && i < time[1][1]) overlap++;
+    if (time[2][0] <= i && i < time[2][1]) overlap++;
+
+    if (overlap === 0) continue;
+    answer += overlap === 1 ? A : overlap === 2 ? B * overlap : C * overlap;
+}
+console.log(answer);
