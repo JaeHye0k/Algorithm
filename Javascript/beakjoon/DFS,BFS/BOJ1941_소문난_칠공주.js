ae82ed0b8424e7d1bd4cc49d02f40a1f8a07d0c6
JaeHye0k <ahhpc2012@naver.com>
2024-02-15 21:41:21 +0900

    코드 정리

--- Javascript/beakjoon/DFS,BFS/BOJ1941_소문난_칠공주.js
+++ Javascript/beakjoon/DFS,BFS/BOJ1941_소문난_칠공주.js
@@ -13,10 +13,10 @@ function bfs(combination) {
     for (const [y, x] of combination) {
         visited[y][x] = false;
     }
-    // 첫 번째 여학생의 위치를 큐에 삽입하고 방문 처리한다.
+    // BFS 시작
     const queue = [combination[0]];
     visited[combination[0][0]][combination[0][1]] = true;
-    let visitCount = 1; // 방문 횟수
+    let visitCount = 1;
     let front = 0;
     while (queue.length > front) {
         let [y, x] = queue[front++];
@@ -31,23 +31,28 @@ function bfs(combination) {
             }
         }
     }
+    // 7명 모두 인접해있을 경우 true 반환
     return visitCount === 7;
 }
 
 // 우선 인접한지 여부 상관없이 7명이 모일수 있는 조합을 전부 구한다. (임도연파가 4명 이상 속해있는 경우는 빼고)
 function dfs(depth, cur, countY) {
-    if (countY >= 4) return; // 임도연파가 4명 이상이라면 재귀 탈출
+    // 임도연파가 4명 이상일 경우
+    if (countY >= 4) return;
+    // 7명이 모였을 경우
     if (depth === 7) {
-        // 7명이 모두 인접해 있을 경우 true를 반환하고, 따라서 카운트가 증가한다.
-        if (bfs(combination)) answer++; // 모든 여학생들이 인접해있다면 카운트
+        if (bfs(combination)) answer++;
         return;
     }
     for (let i = cur; i < 25; i++) {
         let y = Math.floor(i / 5); // 행
         let x = i % 5; // 열
-        combination.push([y, x]); // 현재 여학생의 위치 추가
+        // 현재 학생의 위치 추가
+        combination.push([y, x]);
+        // 재귀 호출
         dfs(depth + 1, i + 1, countY + (students[y][x] === "Y" ? 1 : 0));
-        combination.pop(); // 재귀를 탈출하고 나면 맨 마지막에 저장된 여학생의 위치 제거
+        // 재귀 탈출하면 배열의 마지막 요소 제거(pop)
+        combination.pop();
     }
 }
 
