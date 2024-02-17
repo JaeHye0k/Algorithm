cba2c10789143b9b4b97741592e52463bd79c5a1
JaeHye0k <ahhpc2012@naver.com>
2024-02-17 16:26:21 +0900

    코드 정리

--- Javascript/beakjoon/구현/BOJ2979_트럭주차.js
+++ Javascript/beakjoon/구현/BOJ2979_트럭주차.js
@@ -1,19 +1,16 @@
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./Javascript/input.txt";
-const input = fs.readFileSync(filePath).toString().trim().split("\n");
-const [A, B, C] = input[0].split(" ").map(Number);
-const time = input.slice(1).map((e) => e.split(" ").map(Number));
-
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+const [A, B, C] = input[0].split(' ').map(Number);
+const truck = input.slice(1).map((e) => e.split(' ').map(Number));
 let answer = 0;
-let overlap;
 
 for (let i = 1; i <= 100; i++) {
-    overlap = 0;
-    if (time[0][0] <= i && i < time[0][1]) overlap++;
-    if (time[1][0] <= i && i < time[1][1]) overlap++;
-    if (time[2][0] <= i && i < time[2][1]) overlap++;
-
-    if (overlap === 0) continue;
-    answer += overlap === 1 ? A : overlap === 2 ? B * overlap : C * overlap;
+    let count = 0;
+    truck.forEach(([start, end]) => {
+        if (start <= i && i < end) count += 1;
+    });
+    if (count === 1) answer += A;
+    else if (count === 2) answer += B * count;
+    else if (count === 3) answer += C * count;
 }
 console.log(answer);
