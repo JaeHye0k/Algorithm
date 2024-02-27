081d5d47e8e7e7aab048b73e5ab4c0e192903188
JaeHye0k <ahhpc2012@naver.com>
2024-02-27 23:18:21 +0900

    2차원 DP 테이블을 이용한 방법

--- Javascript/beakjoon/BOJ2629_양팔저울.js
+++ Javascript/beakjoon/BOJ2629_양팔저울.js
@@ -2,26 +2,32 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/inp
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 const N = +input[0];
 const weights = input[1].split(' ').map(Number);
+weights.unshift(0);
 const M = +input[2];
-const marbels = input[3].split(' ').map(Number);
+const marbles = input[3].split(' ').map(Number);
 
-let dp = [0]; // 현재 추 무게 자체도 더할 수 있게 0으로 초기화 -> 0+현재 무게 = 현재 무게
-let answer = [];
-
-// dp에는 추를 사용해서 구슬에 대응할 수 있는 모든 경우를 저장해준다.
-for (let weight of weights) {
-    let tmp = [];
-    for (let i of dp) {
-        tmp.push(i + weight);
-        tmp.push(Math.abs(i - weight));
+const dp = [];
+// 모든 추의 합
+const max = weights.reduce((acc, cur) => (acc += cur), 0);
+// 구슬이 0인 경우, 추의 개수가 0개인 경우를 고려하여 가로, 세로 1칸씩 더 늘려줌
+for (let i = 0; i <= N; i++) {
+    dp.push(Array.from({ length: max + 1 }, () => 'N'));
+}
+// i = 추, j = 구슬
+for (let i = 1; i <= N; i++) {
+    for (let j = 1; j <= max; j++) {
+        // 추를 안올려도 무게가 일치하는 경우
+        if (dp[i - 1][j] === 'Y') dp[i][j] = 'Y';
+        // 추의 무게와 구슬의 무게가 딱 일치하는 경우
+        else if (weights[i] === j) dp[i][j] = 'Y';
+        // 추를 구슬쪽 저울에 올려놓았을 때 무게가 일치하는 경우
+        else if (dp[i - 1][Math.abs(weights[i] - j)] === 'Y') dp[i][j] = 'Y';
+        else if (dp[i - 1][weights[i] + j] === 'Y') dp[i][j] = 'Y';
     }
-
-    dp = Array.from(new Set([...dp, ...tmp])); // 연산 줄이기 위해 중복 제거
 }
-// dp에 구슬과 동일한 무게가 있다면 Y 아니라면 N
-for (let marbel of marbels) {
-    if (dp.includes(marbel)) answer.push('Y');
-    else answer.push('N');
+const answer = [];
+for (let marble of marbles) {
+    if (marble > max) answer.push('N');
+    else answer.push(dp[N][marble]);
 }
-
 console.log(...answer);
