f86d8b3eae580ff3c9e65e74d74c692ec1a44a8b
JaeHye0k <ahhpc2012@naver.com>
2024-03-02 08:47:07 +0900

    코드 수정

--- Javascript/beakjoon/그래프/BOJ22948_원_이동하기_2.js
+++ Javascript/beakjoon/그래프/BOJ22948_원_이동하기_2.js
@@ -1,47 +1,48 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 const N = +input[0];
-const circles = input.slice(1, -1).map((e) => e.split(' ').map(Number));
+const arr = input.slice(1, -1).map((e) => e.split(' ').map(Number));
 const [A, B] = input.at(-1).split(' ').map(Number);
+const visited = Array(N + 1).fill(false);
+
+const circles = [];
+for (let [k, x, r] of arr) {
+    circles.push([k, x - r]);
+    circles.push([-k, x + r]);
+}
+circles.sort((a, b) => a[1] - b[1]);
+
+const stack = [0];
 const tree = Array.from({ length: N + 1 }, () => []);
 
-// 트리 형성 O(N)
-const stack = [0]; // 원의 번호가 저장됨
-const queue = [];
-let front = 0;
-for (let [i, x, r] of circles) {
-    queue.push([x - r, i]);
-    queue.push([x + r, -i]);
-}
-queue.sort((a, b) => a[0] - b[0]);
+makeTree();
+visited[A] = true;
+dfs(A, [A]);
 
-while (queue.length > front) {
-    const [x, num] = queue[front++];
-    if (num < 0) stack.pop();
-    else {
-        tree[num].push(stack.at(-1));
-        tree[stack.at(-1)].push(num);
-        stack.push(num);
+// 괄호쌍 판별 O(n)
+function makeTree() {
+    for (let [k, _] of circles) {
+        if (k < 0) stack.pop();
+        else {
+            tree[stack.at(-1)].push(k);
+            tree[k].push(stack.at(-1));
+            stack.push(k);
+        }
     }
 }
 
-const visited = Array(N + 1).fill(false);
-visited[A] = true;
-dfs(A, [A]); // dfs 수행
-
+// A -> B 경로 찾기
 function dfs(cur, path) {
     if (cur === B) {
         console.log(path.length);
         console.log(...path);
-        return;
-    }
-    for (let next of tree[cur]) {
-        if (!visited[next]) {
-            visited[next] = true;
-            path.push(next);
-            dfs(next, path);
-            visited[next] = false;
-            path.pop();
+    } else {
+        for (let next of tree[cur]) {
+            if (!visited[next]) {
+                visited[next] = true;
+                dfs(next, [...path, next]);
+                visited[next] = false;
+            }
         }
     }
 }
