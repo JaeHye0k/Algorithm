2555547f63fa802bffe9bcc3e4a30de36b91c59b
JaeHye0k <ahhpc2012@naver.com>
2024-03-12 13:27:09 +0900

    해결

--- Javascript/beakjoon/BOJ1520_내리막_길.js
+++ Javascript/beakjoon/BOJ1520_내리막_길.js
@@ -1,50 +1,28 @@
-class Queue {
-    constructor() {
-        this.queue = [];
-        this.front = 0;
-        this.rear = 0;
-    }
-    size() {
-        if (this.queue[this.rear] === undefined) return 0;
-        else return this.rear - this.front + 1;
-    }
-    enqueue(value) {
-        if (this.size()) this.rear++;
-        this.queue[this.rear] = value;
-    }
-    dequeue() {
-        let temp = this.queue[this.front];
-        delete this.queue[this.front];
-        if (this.rear === this.front) {
-            this.front = 0;
-            this.rear = 0;
-        } else this.front++;
-        return temp;
-    }
-}
-
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 const [M, N] = input[0].split(' ').map(Number);
 const arr = input.slice(1).map((e) => e.split(' ').map(Number));
 const dx = [-1, 1, 0, 0];
 const dy = [0, 0, -1, 1];
-let answer = 0;
-bfs(0, 0);
-console.log(answer);
+const dp = Array.from({ length: M }, () => Array(N).fill(-1));
+console.log(dfs(0, 0));
 
-function bfs(x, y) {
-    const queue = new Queue();
-    queue.enqueue([x, y]);
-    while (queue.size()) {
-        const [x, y] = queue.dequeue();
-        if (x === N - 1 && y === M - 1) answer++;
-        for (let i = 0; i < 4; i++) {
-            const nx = x + dx[i];
-            const ny = y + dy[i];
-            if (nx >= 0 && ny >= 0 && nx < N && ny < M && arr[ny][nx] < arr[y][x]) {
-                queue.enqueue([nx, ny]);
-            }
+function dfs(x, y) {
+    // 도착 지점에 도달했을 경우 1(한 가지 경우의 수)리턴
+    if (x === N - 1 && y === M - 1) return 1;
+
+    // 현재 지점이 이미 방문한적 있는 경우 그 위치에서 출발했을 때의 경우의 수 리턴
+    if (dp[y][x] !== -1) return dp[y][x];
+
+    let ways = 0;
+    for (let i = 0; i < 4; i++) {
+        const nx = x + dx[i];
+        const ny = y + dy[i];
+        if (nx >= 0 && ny >= 0 && nx < N && ny < M && arr[ny][nx] < arr[y][x]) {
+            ways += dfs(nx, ny);
         }
     }
+
+    dp[y][x] = ways;
+    return dp[y][x];
 }
