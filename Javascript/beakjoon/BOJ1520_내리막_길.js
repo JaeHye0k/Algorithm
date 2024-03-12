b96dc06519f2c0d76432b7cc36fabfaa5a7efc62
JaeHye0k <ahhpc2012@naver.com>
2024-03-12 10:18:38 +0900

    메모리 초과

--- Javascript/beakjoon/BOJ1520_내리막_길.js
+++ Javascript/beakjoon/BOJ1520_내리막_길.js
@@ -1,18 +1,50 @@
+class Queue {
+    constructor() {
+        this.queue = [];
+        this.front = 0;
+        this.rear = 0;
+    }
+    size() {
+        if (this.queue[this.rear] === undefined) return 0;
+        else return this.rear - this.front + 1;
+    }
+    enqueue(value) {
+        if (this.size()) this.rear++;
+        this.queue[this.rear] = value;
+    }
+    dequeue() {
+        let temp = this.queue[this.front];
+        delete this.queue[this.front];
+        if (this.rear === this.front) {
+            this.front = 0;
+            this.rear = 0;
+        } else this.front++;
+        return temp;
+    }
+}
+
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 const [M, N] = input[0].split(' ').map(Number);
 const arr = input.slice(1).map((e) => e.split(' ').map(Number));
+const dx = [-1, 1, 0, 0];
+const dy = [0, 0, -1, 1];
 let answer = 0;
-dfs(0, 0);
+bfs(0, 0);
 console.log(answer);
 
-function dfs(x, y) {
-    if (x === N - 1 && y === M - 1) {
-        answer++;
-        return;
+function bfs(x, y) {
+    const queue = new Queue();
+    queue.enqueue([x, y]);
+    while (queue.size()) {
+        const [x, y] = queue.dequeue();
+        if (x === N - 1 && y === M - 1) answer++;
+        for (let i = 0; i < 4; i++) {
+            const nx = x + dx[i];
+            const ny = y + dy[i];
+            if (nx >= 0 && ny >= 0 && nx < N && ny < M && arr[ny][nx] < arr[y][x]) {
+                queue.enqueue([nx, ny]);
+            }
+        }
     }
-    if (x - 1 >= 0 && arr[y][x - 1] < arr[y][x]) dfs(x - 1, y);
-    if (x + 1 < N && arr[y][x + 1] < arr[y][x]) dfs(x + 1, y);
-    if (y - 1 >= 0 && arr[y - 1][x] < arr[y][x]) dfs(x, y - 1);
-    if (y + 1 < M && arr[y + 1][x] < arr[y][x]) dfs(x, y + 1);
 }
