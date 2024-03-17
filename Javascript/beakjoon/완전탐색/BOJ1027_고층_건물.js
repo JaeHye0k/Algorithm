c1447fdc4eb2d8fe19550f194add3af4df839da7
JaeHye0k <ahhpc2012@naver.com>
2024-03-17 19:02:21 +0900

    코드 정리

--- Javascript/beakjoon/완전탐색/BOJ1027_고층_건물.js
+++ Javascript/beakjoon/완전탐색/BOJ1027_고층_건물.js
@@ -1,35 +1,23 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 const N = +input[0];
-const buildings = [0].concat(input[1].split(' ').map(Number));
-let answer = Array(N + 1).fill(0);
+const buildings = input[1].split(' ').map(Number);
+const counts = Array(N).fill(0);
 
-for (let i = 1; i <= N; i++) {
-    let rightMax = -Infinity;
-    let leftMin = Infinity;
+for (let i = 0; i < N; i++) {
+    let max = -Infinity;
     const x1 = i;
     const y1 = buildings[i];
-    // 오른쪽 빌딩 탐색
-    for (let j = i; j <= N; j++) {
-        if (i === j) continue;
+    for (let j = i + 1; j < N; j++) {
         const x2 = j;
         const y2 = buildings[j];
         const incline = (y2 - y1) / (x2 - x1);
-        if (incline > rightMax) {
-            answer[i]++;
-            rightMax = incline;
-        }
-    }
-    // 왼쪽 빌딩 탐색
-    for (let j = i; j >= 1; j--) {
-        if (i === j) continue;
-        const x2 = j;
-        const y2 = buildings[j];
-        const incline = (y2 - y1) / (x2 - x1);
-        if (incline < leftMin) {
-            answer[i]++;
-            leftMin = incline;
+        if (incline > max) {
+            max = incline;
+            counts[i]++;
+            counts[j]++;
         }
     }
 }
-console.log(Math.max(...answer));
+
+console.log(Math.max(...counts));
