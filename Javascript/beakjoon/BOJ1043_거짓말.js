938be684599c53041a4447878106b06d46ab317b
JaeHye0k <ahhpc2012@naver.com>
2024-03-18 10:50:39 +0900

    반례

--- Javascript/beakjoon/BOJ1043_거짓말.js
+++ Javascript/beakjoon/BOJ1043_거짓말.js
@@ -6,6 +6,7 @@ const party = input.slice(2).map((e) => e.split(' ').map(Number));
 const truth = new Set(peopleKnowTruth);
 let answer = 0;
 
+// 첫 번째 파티에서 부터 마지막 파티까지 진실을 알고 있는 사람의 집합을 구한다.
 for (let i = 0; i < M; i++) {
     for (let j = 1; j <= party[i][0]; j++) {
         if (truth.has(party[i][j])) {
@@ -14,6 +15,16 @@ for (let i = 0; i < M; i++) {
         }
     }
 }
+// 마지막 파티에서 부터 첫번째 파티까지 진실을 알고 있는 사람의 집합을 구한다.
+for (let i = M - 1; i >= 0; i--) {
+    for (let j = 1; j <= party[i][0]; j++) {
+        if (truth.has(party[i][j])) {
+            party[i].slice(1).forEach((e) => truth.add(e));
+            break;
+        }
+    }
+}
+// 진실을 알고 있는 사람이 포함된 파티라면 카운트하지 않고, 포함되어있지 않다면 카운트한다.
 for (let i = 0; i < M; i++) {
     let known = false;
     for (let j = 1; j <= party[i][0]; j++) {
@@ -27,16 +38,16 @@ for (let i = 0; i < M; i++) {
 console.log(answer);
 
 // 반례
-// input
-// 5 4
-// 1 5
+// 6 5
+// 1 4
 // 2 1 2
 // 2 2 3
 // 2 3 4
-// 2 4 5
+// 2 2 5
+// 2 5 6
 
 // ans
 // 0
 
 // wrong output
-// 2
+// 1
