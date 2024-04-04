668b74e08a5b2a550ef6562b808bb1fc1d70f313
JaeHye0k <ahhpc2012@naver.com>
2024-04-04 20:30:39 +0900

    [Refactoring] 코드 정리

--- Javascript/beakjoon/구현/BOJ14891_톱니바퀴.js
+++ Javascript/beakjoon/구현/BOJ14891_톱니바퀴.js
@@ -1,64 +1,18 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
-let gear = input.slice(0, 4).map((e) => e.split('').map(Number));
-const gearCopy = JSON.parse(JSON.stringify(gear));
+let gear = input.slice(0, 4).map((e) => e.trimEnd().split('').map(Number));
 const k = +input[4];
 const actions = input.slice(5).map((e) => e.split(' ').map(Number));
 let answer = 0;
 
-// 1. 모든 바퀴가 동시에 굴러야 함
-// 2. 방향은 번갈아 가며 구름
-// 3. 극이 같을 경우 반복문 중지
-
-actions.forEach(([num, wise]) => {
+actions.forEach(([num, direction]) => {
     num -= 1;
-    // 시계 방향인 경우
-    if (wise === 1) {
-        let copyWise = wise;
-        for (let i = num; i < 4; i++) {
-            copyWise *= -1;
-            // 오른쪽 톱니바퀴가 존재하고, 극이 다를 경우
-            if (gear[i + 1] && gear[i][2] !== gear[i + 1][6]) {
-                if (copyWise === -1) reverseClockWise(gearCopy[i + 1]);
-                else clockWise(gearCopy[i + 1]);
-            } else break;
-        }
-        copyWise = wise;
-        for (let i = num; i >= 0; i--) {
-            copyWise *= -1;
-            // 왼쪽 톱니바퀴가 존재하고, 극이 다를 경우
-            if (gear[i - 1] && gear[i][6] !== gear[i - 1][2]) {
-                if (copyWise === -1) reverseClockWise(gearCopy[i - 1]);
-                else clockWise(gearCopy[i - 1]);
-            } else break;
-        }
-        clockWise(gearCopy[num]);
-    }
-    // 반시계 방향일 경우
-    else if (wise === -1) {
-        let copyWise = wise;
-        for (let i = num; i < 4; i++) {
-            copyWise *= -1;
-            // 오른쪽 톱니바퀴가 존재하고, 극이 다를 경우
-            if (gear[i + 1] && gear[i][2] !== gear[i + 1][6]) {
-                if (copyWise === 1) clockWise(gearCopy[i + 1]);
-                else reverseClockWise(gearCopy[i + 1]);
-            } else break;
-        }
-        copyWise = wise;
-        for (let i = num; i >= 0; i--) {
-            copyWise *= -1;
-            // 왼쪽 톱니바퀴가 존재하고, 극이 다를 경우
-            if (gear[i - 1] && gear[i][6] !== gear[i - 1][2]) {
-                if (copyWise === 1) clockWise(gearCopy[i - 1]);
-                else reverseClockWise(gearCopy[i - 1]);
-            } else break;
-        }
-        reverseClockWise(gearCopy[num]);
-    }
-    gear = JSON.parse(JSON.stringify(gearCopy));
+    left(num - 1, -direction);
+    right(num + 1, -direction);
+    rotate(gear[num], direction);
 });
 
+// 점수 계산
 for (let i = 0; i < 4; i++) {
     if (gear[i][0] === 0) {
         answer += 0;
@@ -74,7 +28,35 @@ function clockWise(arr) {
     arr.unshift(rear);
 }
 
-function reverseClockWise(arr) {
+function antiClockWise(arr) {
     const front = arr.shift();
     arr.push(front);
+}
+function rotate(arr, direction) {
+    // 시계 방향
+    if (direction === 1) {
+        clockWise(arr);
+    }
+    // 반시계 방향
+    else {
+        antiClockWise(arr);
+    }
+}
+
+function left(num, direction) {
+    if (num < 0) return;
+    // 극이 다른 경우
+    if (gear[num][2] !== gear[num + 1][6]) {
+        left(num - 1, -direction); // 왼쪽 톱니바퀴 탐색
+        rotate(gear[num], direction); // 현재 톱니바퀴 회전
+    }
+}
+
+function right(num, direction) {
+    if (num > 3) return;
+    // 극이 다른 경우
+    if (gear[num][6] !== gear[num - 1][2]) {
+        right(num + 1, -direction); // 오른쪽 톱니바퀴 탐색
+        rotate(gear[num], direction); // 현재 톱니바퀴 회전
+    }
 }
