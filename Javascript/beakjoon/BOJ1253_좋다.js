c95b7048ad79edb851ca150b4a9d0359138e1738
JaeHye0k <ahhpc2012@naver.com>
2024-04-11 14:19:32 +0900

    [cheat] two pointer

--- Javascript/beakjoon/BOJ1253_좋다.js
+++ Javascript/beakjoon/BOJ1253_좋다.js
@@ -1,23 +1,29 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 const N = +input[0];
-const arr = input[1].split(' ').map(Number);
-const sum = {};
-for (let i = 0; i < N; i++) {
-    for (let j = 0; j < N; j++) {
-        if (i === j) continue;
-        sum[arr[i] + arr[j]] = true;
-    }
-}
-
+const arr = input[1]
+    .split(' ')
+    .map(Number)
+    .sort((a, b) => a - b);
 let answer = 0;
+
 for (let i = 0; i < N; i++) {
-    if (sum[arr[i]]) answer++;
+    answer += binarySearch(i);
 }
 console.log(answer);
 
-// 반례
-// 2
-// 0 2
-// correct = 0
-// wrong = 1
+function binarySearch(target) {
+    let left = 0;
+    let right = N - 1;
+    while (left < right) {
+        const sum = arr[left] + arr[right];
+        if (sum === arr[target]) {
+            // target이 sum에 포함되면 안됨
+            if (left === target) left++;
+            else if (right === target) right--;
+            else return 1;
+        } else if (sum > arr[target]) right--;
+        else left++;
+    }
+    return 0;
+}
