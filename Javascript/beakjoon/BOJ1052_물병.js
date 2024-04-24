8b9f278e9617746cdbbb0d39f959dc6b09959ca3
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-04-25 00:47:02 +0900

    그리디 해결

--- Javascript/beakjoon/BOJ1052_물병.js
+++ Javascript/beakjoon/BOJ1052_물병.js
@@ -1,21 +1,41 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
 const [N, K] = require('fs').readFileSync(filePath).toString().trim().split(' ').map(Number);
+const remains = [];
+let bottles = N;
+let litter = 1;
 let answer = 0;
 
-let n = N;
-function countOne(num) {
-    let count = 0;
-    while (num > 0) {
-        if (num % 2 === 1) {
-            count++;
-        }
-        num = num >> 1;
+while (bottles >= 1) {
+    if (bottles % 2 === 0) {
+        bottles /= 2;
+        litter *= 2;
+    } else {
+        remains.push(litter);
+        bottles -= 1;
     }
-    return count;
 }
 
-while (countOne(n) > K) {
-    n++; // 상점에서 물병 한 개 추가
-    answer++;
+if (bottles + remains.length <= K) {
+    console.log(answer);
+    process.exit();
 }
+
+for (let i = 0; i < remains.length - 1; i++) {
+    if (bottles + remains.length <= K)
+        if (remains[i] !== remains[i + 1]) {
+            recursion(i);
+        }
+    remains[i + 1] += remains[i];
+    bottles++;
+    if (remains.length - bottles <= K) break;
+}
+
+// remains[index] 와 remains[index+1] 을 같은 값으로 만든다.
+function recursion(index) {
+    answer += remains[index];
+    remains[index] *= 2;
+    if (remains[index] === remains[index + 1]) return;
+    recursion(index);
+}
+
 console.log(answer);
