af8667a8805fa9e5d0ca41eade33fe8f084a05d9
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-04-25 00:30:41 +0900

    해결

--- Javascript/beakjoon/BOJ1052_물병.js
+++ Javascript/beakjoon/BOJ1052_물병.js
@@ -1,36 +1,21 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
 const [N, K] = require('fs').readFileSync(filePath).toString().trim().split(' ').map(Number);
-const remains = [];
-let bottles = N;
-let litter = 1;
 let answer = 0;
 
-while (bottles >= 1) {
-    if (bottles + remains.length <= K) break;
-    if (bottles % 2 === 0) {
-        bottles /= 2;
-        litter *= 2;
-    } else {
-        remains.push(litter);
-        bottles -= 1;
+let n = N;
+function countOne(num) {
+    let count = 0;
+    while (num > 0) {
+        if (num % 2 === 1) {
+            count++;
+        }
+        num = num >> 1;
     }
+    return count;
 }
 
-for (let i = 0; i < remains.length - 1; i++) {
-    if (remains[i] !== remains[i + 1]) {
-        recursion(i);
-    }
-    remains[i + 1] += remains[i];
-    bottles++;
-    if (remains.length - bottles <= K) break;
+while (countOne(n) > K) {
+    n++; // 상점에서 물병 한 개 추가
+    answer++;
 }
-
-// remains[index] 와 remains[index+1] 을 같은 값으로 만든다.
-function recursion(index) {
-    answer += remains[index];
-    remains[index] *= 2;
-    if (remains[index] === remains[index + 1]) return;
-    recursion(index);
-}
-
 console.log(answer);
