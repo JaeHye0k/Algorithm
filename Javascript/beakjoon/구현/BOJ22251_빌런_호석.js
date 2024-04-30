e4972c1c9044c6e56ab08bca5ff57c072935cfe1
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-04-30 20:18:30 +0900

    구현 (개선)

--- Javascript/beakjoon/구현/BOJ22251_빌런_호석.js
+++ Javascript/beakjoon/구현/BOJ22251_빌런_호석.js
@@ -16,49 +16,28 @@ const led = [
     [1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 0, 1, 1],
 ];
-
-const graph = Array.from({ length: 10 }, () => []);
-let count;
+const costs = Array.from({ length: 10 }, () => Array(10).fill(0));
 for (let i = 0; i <= 9; i++) {
     for (let j = 0; j <= 9; j++) {
-        count = 0;
+        let count = 0;
         for (let k = 0; k < 7; k++) {
             if (led[i][k] !== led[j][k]) {
                 count++;
             }
         }
-        graph[i][j] = count;
+        costs[i][j] = count;
     }
 }
 
-// i로 target을 만들 수 있는지 확인
-function checkMakable(target, cur) {
-    let count = 0;
-    for (let i = 0; i < target.length; i++) {
-        count += graph[target[i]][cur[i]];
-    }
-    // 반전 시킬 수 있는 최대 개수보다 많이 반전시켰다면
-    if (count > P || count === 0) return false;
-    return true;
-}
-
-// 자릿수 맞춰서 리턴해주는 함수
-function getDigit(curFloor) {
-    curFloor = String(curFloor).split('');
-    const len = curFloor.length;
-    const arr = Array(K - len).fill(0);
-    // 자릿수 맞추기
-    for (let i = 0; i < len; i++) {
-        arr.push(+curFloor[i]);
-    }
-    return arr;
-}
-
-const currentFloor = getDigit(X);
 let answer = 0;
 for (let i = 1; i <= N; i++) {
-    if (checkMakable(currentFloor, getDigit(i))) {
-        answer++;
+    let cost = 0;
+    let [a, b] = [X, i];
+    for (let j = 0; j < K; j++) {
+        cost += costs[a % 10][b % 10];
+        a = Math.floor(a / 10);
+        b = Math.floor(b / 10);
     }
+    if (cost <= P && cost > 0) answer++;
 }
 console.log(answer);
