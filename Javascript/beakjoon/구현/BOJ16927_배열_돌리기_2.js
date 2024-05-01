a45d7d346097275b9af2644a819bb1620ab1ca1d
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-05-01 15:46:20 +0900

    길이를 미리 구해줄 필요 없어서 뺌

--- Javascript/beakjoon/구현/BOJ16927_배열_돌리기_2.js
+++ Javascript/beakjoon/구현/BOJ16927_배열_돌리기_2.js
@@ -1,11 +1,3 @@
-// 일일히 회전시킨다면 최대 10^9번 회전시켜야 하기 떄문에 시간 초과
-// [y][x] 위치에서 R번 회전시켰을 때의 위치를 구한 뒤, 그 위치에 [y][x]에 있는 값 넣기
-
-// 배열 외곽의 길이 = (N-1)*2 + (M-1)*2
-// (N-1)*2 + (M-1)*2 = 0 일 경우, 길이 = 1
-// 안쪽으로 들어가면 N -= 2, M -= 2
-// i += R % (N-1)*2 + (M-1)*2
-
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 const [N, M, R] = input[0].split(' ').map(Number);
@@ -13,9 +5,8 @@ const arr = input.slice(1).map((e) => e.split(' ').map(Number));
 const answerArr = Array.from({ length: N }, () => Array(M).fill(0));
 let [n, m] = [N, M];
 
-function makeArr(len, lt, rb) {
+function makeArr(lt, rb) {
     const row = [];
-    if (len === 1) row.push(arr[lt[0]][lt[1]]);
     // 하
     for (let i = lt[0]; i < rb[0]; i++) {
         row.push(arr[i][lt[1]]);
@@ -32,6 +23,8 @@ function makeArr(len, lt, rb) {
     for (let i = rb[1]; i > lt[1]; i--) {
         row.push(arr[lt[0]][i]);
     }
+    if (row.length === 0) row.push(arr[lt[0]][lt[1]]);
+
     return row;
 }
 
@@ -69,13 +62,17 @@ function getArrAfterRotate(row, lt, rb) {
 
 let count = 0;
 while (n > 0 && m > 0) {
-    let len = (n - 1) * 2 + (m - 1) * 2;
-    if (len === 0) len = 1;
+    // 왼쪽 위 좌표
     const lt = [count, count];
+    // 오른쪽 아래 좌표
     const rb = [N - 1 - count, M - 1 - count];
-    const row = makeArr(len, lt, rb);
-    const newRow = getRowAfterRotate(row, len);
+    // 외곽을 감싼 배열을 1차원 배열로 변환
+    const row = makeArr(lt, rb);
+    // 1차원 배열에 회전 적용시키기
+    const newRow = getRowAfterRotate(row, row.length);
+    // 1차원 배열을 2차원 배열로 만들기
     getArrAfterRotate(newRow, lt, rb);
+
     n -= 2;
     m -= 2;
     count++;
