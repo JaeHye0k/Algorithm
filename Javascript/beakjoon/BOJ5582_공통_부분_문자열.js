11cfa9f94b6e460d2d36d2da0db6f5e610762c4d
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-05-07 18:08:48 +0900

    DP 개선

--- Javascript/beakjoon/BOJ5582_공통_부분_문자열.js
+++ Javascript/beakjoon/BOJ5582_공통_부분_문자열.js
@@ -1,14 +1,15 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
 const [str1, str2] = require('fs').readFileSync(filePath).toString().trim().split('\n');
-const LCS = Array.from({ length: str1.length + 1 }, () => Array(str2.length + 1).fill(0));
 let answer = 0;
 
-for (let i = 0; i < str1.length; i++) {
-    for (let j = 0; j < str2.length; j++) {
-        if (str1[i] === str2[j]) {
-            LCS[i + 1][j + 1] = LCS[i][j] + 1;
-            answer = LCS[i + 1][j + 1] > answer ? LCS[i + 1][j + 1] : answer;
-        } else LCS[i + 1][j + 1] = 0;
+const dp = Array(str1.length).fill(0);
+for (let i = 0; i < str2.length; i++) {
+    for (let j = str1.length - 1; j >= 0; j--) {
+        if (str2[i] === str1[j]) {
+            if (j === 0) dp[j] = 1;
+            else dp[j] = dp[j - 1] + 1;
+            answer = dp[j] > answer ? dp[j] : answer;
+        } else dp[j] = 0;
     }
 }
 
