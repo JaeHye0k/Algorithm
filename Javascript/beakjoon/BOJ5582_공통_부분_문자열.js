c8773b2997c5a81d1dc15f9200fdbc0f321d4af4
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-05-07 17:22:03 +0900

    DP

--- Javascript/beakjoon/BOJ5582_공통_부분_문자열.js
+++ Javascript/beakjoon/BOJ5582_공통_부분_문자열.js
@@ -1,20 +1,14 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
 const [str1, str2] = require('fs').readFileSync(filePath).toString().trim().split('\n');
+const LCS = Array.from({ length: str1.length + 1 }, () => Array(str2.length + 1).fill(0));
 let answer = 0;
-
-function recursion(i, j, str) {
-    if (str1[i] === str2[j]) {
-        recursion(i + 1, j + 1, str + str1[i]);
-    } else {
-        answer = str.length > answer ? str.length : answer;
-    }
-}
 
 for (let i = 0; i < str1.length; i++) {
     for (let j = 0; j < str2.length; j++) {
         if (str1[i] === str2[j]) {
-            recursion(i, j, '');
-        }
+            LCS[i + 1][j + 1] = LCS[i][j] + 1;
+            answer = LCS[i + 1][j + 1] > answer ? LCS[i + 1][j + 1] : answer;
+        } else LCS[i + 1][j + 1] = 0;
     }
 }
 
