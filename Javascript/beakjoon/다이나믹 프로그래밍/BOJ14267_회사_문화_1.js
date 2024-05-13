de72f89c3fb04a049a289974cb11d80d319adbb0
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-05-13 20:44:42 +0900

    DP

--- Javascript/beakjoon/다이나믹 프로그래밍/BOJ14267_회사_문화_1.js
+++ Javascript/beakjoon/다이나믹 프로그래밍/BOJ14267_회사_문화_1.js
@@ -1,7 +1,7 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 const [n, m] = input[0].split(' ').map(Number);
-const superior = input[1].split(' ').map(Number);
+const superior = [0].concat(input[1].split(' ').map(Number));
 const employee = input.slice(2, 2 + m).map((e) => e.split(' ').map(Number));
 const compliment = Array(n + 1).fill(0);
 
@@ -9,8 +9,8 @@ for (const [i, w] of employee) {
     compliment[i] += w;
 }
 
-for (let i = 1; i < n; i++) {
-    compliment[i + 1] += compliment[superior[i]];
+for (let i = 2; i <= n; i++) {
+    compliment[i] += compliment[superior[i]];
 }
 
 compliment.shift();
