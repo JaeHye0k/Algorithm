afdd03f72c85c72568918b4e16b45cd8aa213be5
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-05-22 10:41:25 +0900

    DFS, 백트래킹

--- Javascript/beakjoon/그래프/BOJ1987_알파벳.js
+++ Javascript/beakjoon/그래프/BOJ1987_알파벳.js
@@ -1,30 +1,25 @@
-// 그동안 지나온 칸에 대한 정보 Map에 저장
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 const [R, C] = input[0].split(' ').map(Number);
-const board = input.slice(1, 1 + R);
-const visited = Array.from({ length: R }, () => Array(C).fill(false));
+const board = input.slice(1, 1 + R).map((e) => e.split('').map((e) => e.charCodeAt() - 65));
+const visited = Array(26).fill(false);
 const dx = [-1, 1, 0, 0];
 const dy = [0, 0, -1, 1];
-const map = new Map();
 let answer = 0;
 
 function dfs(y, x, count) {
-    const alpha = board[y][x];
     answer = Math.max(answer, count);
     for (let i = 0; i < 4; i++) {
         const nx = x + dx[i];
         const ny = y + dy[i];
-        if (nx >= 0 && ny >= 0 && nx < C && ny < R && !visited[ny][nx] && !map.has(board[ny][nx])) {
-            visited[ny][nx] = true;
-            map.set(board[ny][nx], 1);
+        if (nx >= 0 && ny >= 0 && nx < C && ny < R && !visited[board[ny][nx]]) {
+            visited[board[ny][nx]] = true;
             dfs(ny, nx, count + 1);
-            map.delete(board[ny][nx]);
-            visited[ny][nx] = false;
+            visited[board[ny][nx]] = false;
         }
     }
 }
 
-map.set(board[0][0]);
+visited[board[0][0]] = true;
 dfs(0, 0, 1);
 console.log(answer);
