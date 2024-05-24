41e1720083dbee5bff97fca51814140c8e414b73
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-05-24 19:58:24 +0900

    수학 (스스로)

--- Javascript/beakjoon/수학/BOJ2436_공약수.js
+++ Javascript/beakjoon/수학/BOJ2436_공약수.js
@@ -3,26 +3,29 @@ const [a, b] = require('fs').readFileSync(filePath).toString().trim().split(' ')
 let min = Infinity;
 let answer = '';
 
-let x;
-let y;
+let x, y, n, m;
+const right = b / a;
 
+// x, y가 서로소인지 판별
 function check(x, y) {
     let bigger = x > y ? x : y;
     for (let i = 2; i < bigger / 2; i++) {
-        // x와 y가 같은 수로 나누어 떨어지면 최소공배수를 구하기 위해 사용할 수 없음
+        // x,y가 같은 수로 나누어떨어질 경우 서로소가 아님
         if (x % i === 0 && y % i === 0) return false;
     }
     return true;
 }
-for (let i = 1; i <= b / a / 2 + 1; i++) {
+for (let i = 1; i <= right / 2 + 1; i++) {
     y = i;
-    x = b / a / y;
+    x = right / y;
     // x가 소수일 때는 무시
     if (x - Math.floor(x) > 0) continue;
-    if (x * y === b / a && check(x, y)) {
-        if (a * x + a * y < min) {
-            min = a * x + a * y;
-            answer = a * y + ' ' + a * x;
+    if (x * y === right && check(x, y)) {
+        n = a * x;
+        m = a * y;
+        if (n + m < min) {
+            min = n + m;
+            answer = m + ' ' + n;
         }
     }
 }
