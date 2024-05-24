0f5b8e881f9813e2bcb7d4d43cfa8b7e69ddf7b0
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-05-24 21:41:59 +0900

    수학 (속도 개선)

--- Javascript/beakjoon/수학/BOJ2436_공약수.js
+++ Javascript/beakjoon/수학/BOJ2436_공약수.js
@@ -9,13 +9,13 @@ const right = b / a;
 // x, y가 서로소인지 판별
 function check(x, y) {
     let bigger = x > y ? x : y;
-    for (let i = 2; i < bigger / 2; i++) {
+    for (let i = 2; i < Math.sqrt(bigger); i++) {
         // x,y가 같은 수로 나누어떨어질 경우 서로소가 아님
         if (x % i === 0 && y % i === 0) return false;
     }
     return true;
 }
-for (let i = 1; i <= right / 2 + 1; i++) {
+for (let i = 1; i <= Math.sqrt(right); i++) {
     y = i;
     x = right / y;
     // x가 소수일 때는 무시
