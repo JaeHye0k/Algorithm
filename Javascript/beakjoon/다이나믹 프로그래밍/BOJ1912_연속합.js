535c5bd8035970e4642b3bd8478900416a63db57
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-05-28 22:57:02 +0900

    DP

--- Javascript/beakjoon/다이나믹 프로그래밍/BOJ1912_연속합.js
+++ Javascript/beakjoon/다이나믹 프로그래밍/BOJ1912_연속합.js
@@ -1,24 +1,10 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+const n = +input[0];
+const arr = input[1].split(' ').map(Number);
 
-function solution(n, arr) {
-    let dp = arr.map((e) => e);
-    for (let i = 1; i < n; i++) {
-        dp[i] = Math.max(dp[i], dp[i] + dp[i - 1], dp[i] + arr[i - 1]);
-    }
-    console.log(Math.max(...dp));
+let dp = arr.map((e) => e);
+for (let i = 1; i < n; i++) {
+    dp[i] = Math.max(dp[i], dp[i] + dp[i - 1], dp[i] + arr[i - 1]);
 }
-
-let input = [];
-
-rl.on("line", (line) => {
-    input.push(line);
-}).on("close", () => {
-    const n = Number(input[0]);
-    const arr = input[1].split(" ").map(Number);
-    solution(n, arr);
-    process.exit();
-});
+console.log(Math.max(...dp));
