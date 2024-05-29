580ffeea4430e6b0c2e47edea433b984b384d540
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-05-29 23:50:17 +0900

    구현

--- Javascript/beakjoon/구현/BOJ2469_사다리_타기.js
+++ Javascript/beakjoon/구현/BOJ2469_사다리_타기.js
@@ -1,49 +1,53 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
-
-// 가로 줄 두 개가 연속으로 붙는 경우는 없다.
-// . A 부터 탐색
-// . i=세로, j=가로 일때, ladder[i][j-1]왼쪽으로 가는 방향, ladder[i][j]오른쪽으로 가는 방향 확인
-// . '*' 이라면 i+1, '-'이라면 (j-1혹은 j+1) 그리고 i+1, '?'라면 기본적으로 i+1
-// . 끝에 도달했을 때 ladder[i][j] 가 출발했을 때의 알파벳과 동일하다면 [j]번째 ?는 '*'
-// . 다르다면 ?를 만난 지점까지 재귀를 탈출해서 그때의 [j]위치에 있는 ?를 '-'로 바꾼뒤 다시 탐색. [j-1]이 이미 '-'라면 못 바꿈
-// . 끝에 도달했을 때 ladder[i][j] 가 출발했을 때의 알파벳과 동일하다면 [j]번째 ?는 '-'
-// . 다르다면 ?를 만난 지점까지 재귀를 탈출해서 [j]를 '*'로 바꾸고 [j-1]위치에 있는 ?를 '-'로 바꾼뒤 다시 탐색. [j-2]이 이미 '-'라면 못 바꿈
-// . └─ 하지만 -로 바꿀 시 이미 지나온 사다리에도 영향을 줄 수 있지 않나?
-let answer = '';
 const k = +input[0];
 const n = +input[1];
-const expected = input[2].trimEnd();
+const expected = input[2].trimEnd().split('');
 const ladder = input.slice(3, 3 + n).map((e) => e.trimEnd().split(''));
 
-for (let i = 0; i < k; i++) {
-    dfs(i, 0, i, 0, 0, 0);
+// 가로 줄 두 개가 연속으로 붙는 경우는 없다.
+// 1. 길이 k의 start, finish 배열 생성
+// 2. 가려진 행이 i라고 했을때, start는 0 ~ i-1 까지 1씩 증가, finish는 n-1 ~ i+1 까지 1씩 감소
+// 3. ladder[j] === '-'라면 swap([j],[j+1])
+
+let hiddenIdx = 0;
+for (let i = 0; i < n; i++) {
+    if (ladder[i][0] === '?') {
+        hiddenIdx = i;
+        break;
+    }
 }
 
-function dfs(x1, y1, alphabet, x2, y2, d) {
-    if (y1 === n) {
-        if (expected[x1] === String.fromCodePoint(alphabet + 65) && d === 0) {
-            answer += '*';
-            return true;
-        } else if (expected[x1] === String.fromCodePoint(alphabet + 65) && d === 1) {
-            answer += '-';
-            ladder[y2][x2] = '-';
-            return true;
+const start = Array.from(Array(k), (_, i) => String.fromCodePoint(65 + i));
+const finish = expected.map((e) => e);
+
+for (let i = 0; i < hiddenIdx; i++) {
+    for (let j = 0; j < k; j++) {
+        if (ladder[i][j] === '-') {
+            [start[j], start[j + 1]] = [start[j + 1], start[j]];
         }
     }
+}
 
-    if (ladder[y1][x1] === '?') {
-        x2 = x1;
-        y2 = y1;
-        ladder[y2][x2] = '*';
+for (let i = n - 1; i > hiddenIdx; i--) {
+    for (let j = 0; j < k; j++) {
+        if (ladder[i][j] === '-') {
+            [finish[j], finish[j + 1]] = [finish[j + 1], finish[j]];
+        }
     }
+}
 
-    if (ladder[y1][x1] === '*' && dfs(x1, y1 + 1, alphabet, x2, y2)) return true; // mid = 0
-    else {
-        d++;
+let answer = '';
+for (let i = 0; i < k - 1; i++) {
+    if (start[i] === finish[i]) {
+        answer += '*';
+    } else if (start[i] === finish[i + 1] && start[i + 1] === finish[i]) {
+        answer += '-';
+        [start[i], start[i + 1]] = [start[i + 1], start[i]];
+    } else {
+        answer = 'x'.repeat(k - 1);
+        break;
     }
-    if (ladder[y1][x1] === '-' && dfs(x1 + 1, y1 + 1, alphabet, x2, y2)) return true; // right = 1
-    if (ladder[y1][x1 - 1] === '-' && dfs(x1 - 1, y1 + 1, alphabet, x2, y2)) return true; // left = 2
 }
 
 console.log(answer);
