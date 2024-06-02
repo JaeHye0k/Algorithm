c18e89325dddab06c242c344976f7c19a36f79fc
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-06-02 10:36:06 +0900

    구현

--- Javascript/beakjoon/구현/BOJ15686_치킨_배달.js
+++ Javascript/beakjoon/구현/BOJ15686_치킨_배달.js
@@ -1,41 +1,39 @@
-// 빈칸 0,  집 1, 치킨집 2
-// 집부터 가장 가까운 치킨집까지의 거리 = 치킨 거리
-// 도시의 치킨 거리 = 모든 집의 치킨 거리의 합
-// 모든 집부터 M개의 치킨집 까지의 거리의 총 합이 최소가 되도록 만드시오.
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 const [N, M] = input[0].split(' ').map(Number);
-const arr = input.slice(1).map((row) => row.split(' ').map(Number));
-const house = [];
-const chicken = [];
+const arr = input.slice(1).map((e) => e.split(' ').map(Number));
 let answer = Infinity;
 
-function combination(cur, chickens) {
-    // 치킨집이 M개가 되었을 경우
-    // 모든 집부터 M개의 치킨집 까지의 치킨 거리를 구함
-    if (chickens.length === M) {
-        let chickenDistOfCity = 0; // 도시의 치킨 거리
-        for (const [hy, hx] of house) {
-            let chickenDist = Infinity; // 현재 집의 치킨 거리
-            for (const num of chickens) {
-                const [cy, cx] = chicken[num];
-                const dist = Math.abs(hy - cy) + Math.abs(hx - cx);
-                chickenDist = Math.min(chickenDist, dist);
-            }
-            chickenDistOfCity += chickenDist;
-        }
-        answer = Math.min(answer, chickenDistOfCity);
-    }
-
-    for (let i = cur; i < chicken.length; i++) {
-        combination(i + 1, [...chickens, i]);
-    }
-}
-
+const chickens = [];
+const house = [];
 for (let i = 0; i < N; i++) {
     for (let j = 0; j < N; j++) {
         if (arr[i][j] === 1) house.push([i, j]);
-        else if (arr[i][j] === 2) chicken.push([i, j]);
+        if (arr[i][j] === 2) chickens.push([i, j]);
+    }
+}
+
+// 치킨집 M개를 골랐을 때 도시의 치킨거리중 가장 작은값이 정답
+function caculateDist(arr) {
+    let dist = 0;
+    let min = Infinity;
+    for (const [y1, x1] of house) {
+        for (const [y2, x2] of arr) {
+            min = Math.min(Math.abs(y1 - y2) + Math.abs(x1 - x2), min);
+        }
+        dist += min;
+        min = Infinity;
+    }
+    answer = Math.min(dist, answer);
+}
+
+function combination(cur, arr) {
+    if (arr.length === M) {
+        caculateDist(arr);
+        return;
+    }
+    for (let i = cur; i < chickens.length; i++) {
+        combination(i + 1, [...arr, chickens[i]]);
     }
 }
 
