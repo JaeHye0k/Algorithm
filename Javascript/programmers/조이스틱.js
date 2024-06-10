e0b258f5dc1d5ac8253595c2251c8099469da5d7
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-06-10 11:27:42 +0900

    100점

--- Javascript/programmers/조이스틱.js
+++ Javascript/programmers/조이스틱.js
@@ -1,46 +1,27 @@
 function solution(name) {
     let answer = 0;
-    let str = 'A'.repeat(name.length);
-    const visited = Array(name.length).fill(false);
-    let curIdx = 0;
+    let min = name.length - 1; // 최소 이동 횟수
 
-    while (str !== name) {
-        let closeDist = Infinity;
-        let closeIdx = curIdx;
-        // 가장 가까운 인덱스 찾기
-        for (let i = 0; i < name.length; i++) {
-            if (name[i] !== 'A' && !visited[i]) {
-                const dist = getDist(curIdx, i, name.length);
-                if (dist < closeDist) {
-                    closeDist = dist;
-                    closeIdx = i;
-                }
-            }
-        }
-        curIdx = closeIdx; // 가장 가까운 인덱스로 이동
-        answer += closeDist; // 이동 횟수 누적
-        visited[curIdx] = true;
-
-        const from = str[curIdx].codePointAt() - 65;
-        const to = name[curIdx].codePointAt() - 65;
-        const dist = getDist(from, to, 26); // 두 알파벳 사이의 최단 거리
+    for (let i = 0; i < name.length; i++) {
+        const curAlphabet = name[i].codePointAt() - 65;
+        const dist = getDist(0, curAlphabet, 26);
         answer += dist;
-        str = str.slice(0, curIdx) + name[curIdx] + str.slice(curIdx + 1); // 알파벳 변경
+
+        let nextIdx = i + 1;
+        // A가 아닌곳을 찾을 때까지 nextIdx 증가
+        while (nextIdx < name.length && name[nextIdx] === 'A') nextIdx++;
+
+        min = Math.min(min, i * 2 + name.length - nextIdx, i + (name.length - nextIdx) * 2);
     }
+    answer += min;
 
     // 거리 구하는 함수
-    function getDist(from, to, n) {
+    function getDist(from, to, len) {
         const diff = Math.abs(from - to);
-        return Math.min(diff, n - diff);
+        return Math.min(diff, len - diff);
     }
 
     return answer;
 }
 
 console.log(solution('ABABAAAAABA'));
-
-// 반례
-// ABABAAAAABA
-// answer: 10
-
-// ← ← ↑ → → → ↑ → → ↑
