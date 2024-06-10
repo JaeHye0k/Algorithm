8183ef6fe178a52e2c59695e4f744be96f359f7b
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-06-10 11:12:18 +0900

    리팩터링 (74.1점)

--- Javascript/programmers/조이스틱.js
+++ Javascript/programmers/조이스틱.js
@@ -37,36 +37,7 @@ function solution(name) {
     return answer;
 }
 
-// 오른쪽으로만 이동하거나, 왼쪽으
-function AnswerSolution(name) {
-    var answer = 0;
-    let min = name.length - 1;
-
-    for (let i = 0; i < name.length; i++) {
-        let currentAlphabet = name.charCodeAt(i);
-
-        if (currentAlphabet < 78) {
-            answer += currentAlphabet % 65;
-        } else {
-            answer += 91 - currentAlphabet;
-        }
-
-        let nextIndex = i + 1;
-
-        while (nextIndex < name.length && name[nextIndex] === 'A') {
-            nextIndex += 1;
-        }
-        min = Math.min(
-            min,
-            i * 2 + name.length - nextIndex, // 먼저 오른쪽으로 가기
-            i + (name.length - nextIndex) * 2 // 처음부터 반대로 가기
-        );
-    }
-    answer += min;
-    return answer;
-}
 console.log(solution('ABABAAAAABA'));
-console.log(AnswerSolution('ABABAAAAABA'));
 
 // 반례
 // ABABAAAAABA
