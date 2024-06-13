54d56fdc673bbac9fd2289d2de4b06e644c9e977
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-06-13 09:04:10 +0900

    리팩터링 (주석)

--- Javascript/beakjoon/구현/BOJ15683_감시.js
+++ Javascript/beakjoon/구현/BOJ15683_감시.js
@@ -16,19 +16,19 @@ rl.on('line', (line) => {
 });
 
 function solution(N, M, arr) {
-    const cctv = [];
-    let count = 0; // 사각지대의 개수
+    const cctv = []; // cctv의 좌표
+    let count = 0; // 사각지대(0)의 개수
     for (let i = 0; i < N; i++) {
         for (let j = 0; j < M; j++) {
             if (arr[i][j] >= 1 && arr[i][j] <= 5) {
-                cctv.push([i, j, arr[i][j]]); // y,x,cctv번호,0 초기 방향
+                cctv.push([i, j, arr[i][j]]); // y,x,cctv번호
             }
             if (arr[i][j] === 0) count++;
         }
     }
 
     const cctvCount = cctv.length; // cctv의 개수
-    const cctvDir = Array(cctvCount).fill(0); // 각각의 cctv의 방향
+    const rotated = Array(cctvCount).fill(0); // 각각의 cctv의 회전상태 (0 ~ 3)
 
     const dx = [1, 0, -1, 0];
     const dy = [0, 1, 0, -1];
@@ -36,27 +36,35 @@ function solution(N, M, arr) {
     combination(0, 0);
     console.log(count);
 
+    // cctv의 회전 상태의 모든 경우의 수
+    // cctv의 개수가 5개라고 가정했을 때 아래와 같이 진행됨
+    // rotated = [0, 0, 0, 0, 0]
+    // rotated = [0, 0, 0, 0, 1]
+    // rotated = [0, 0, 0, 0, 2]
+    // rotated = [0, 0, 0, 0, 3]
+    // rotated = [0, 0, 0, 1, 0]
+    // rotated = [0, 0, 0, 1, 1]
     function combination(cur) {
         if (cur === cctvCount) {
-            // 현재 cctvDir을 이용해서 탐색 시작
-            bfs();
+            // 현재 회전 정보를 이용해서 탐색 시작
+            start();
             return;
         }
         for (let i = cur; i < cctvCount; i++) {
             for (let j = 0; j < 4; j++) {
-                cctvDir[i] = j; // i번 cctv의 방향은 j
+                rotated[i] = j; // i번 cctv의 방향은 j
                 combination(i + 1);
             }
         }
     }
-    // 각각의 cctv에서
-    function bfs() {
+    // 각각의 cctv의 방향에 맞게 #으로 채우기
+    function start() {
         const cctvArr = cctv.map((e) => [...e]);
         const newArr = arr.map((e) => [...e]);
 
         for (let i = 0; i < cctvCount; i++) {
             const [y, x, num] = cctvArr[i];
-            const d = cctvDir[i];
+            const d = rotated[i];
             if (num === 1) {
                 dfs(y, x, dy[d], dx[d], newArr);
             } else if (num === 2) {
@@ -79,6 +87,7 @@ function solution(N, M, arr) {
         count = Math.min(count, getCount(newArr));
     }
 
+    // 한 방향을 #으로 채우기
     function dfs(y, x, dy, dx, newArr) {
         const ny = y + dy;
         const nx = x + dx;
@@ -88,6 +97,7 @@ function solution(N, M, arr) {
         }
     }
 
+    // 사각지대의 개수 카운트
     function getCount(arr) {
         let count = 0;
         for (let i = 0; i < N; i++) {
@@ -118,14 +128,3 @@ function solution(N, M, arr) {
 // 4-3 하(1,0) 좌(0,-1) 상(-1,0)
 
 // 5-0,1,2,3 우(0,1) 하(1,0) 좌(0,-1) 상(-1,0)
-
-// 0 0 0 0 0
-// 0 0 0 0 1
-// 0 0 0 0 2
-// 0 0 0 0 3
-// 0 0 0 1 0
-// 0 0 0 1 1
-// 벽 통과 x, cctv 통과 o,
-// 90도 회전 가능
-// 방향을 전달하면 해당 방향으로 끝까지 가는 함수 (범위를 벗어나거나 arr[y][x] === 6일 경우 탐색 종료)
-//
