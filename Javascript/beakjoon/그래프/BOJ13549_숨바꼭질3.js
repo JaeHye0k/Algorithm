67470a8848b7c1b017e066e1e2dee81c343f2fb0
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-06-20 15:41:25 +0900

    BFS

--- Javascript/beakjoon/그래프/BOJ13549_숨바꼭질3.js
+++ Javascript/beakjoon/그래프/BOJ13549_숨바꼭질3.js
@@ -1,64 +1,33 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
 const [N, K] = require('fs').readFileSync(filePath).toString().trim().split(' ').map(Number);
-const distance = Array(10 ** 5 + 1).fill(Infinity);
+const visited = Array(10 ** 5 + 1).fill(false);
 
-class PriorityQueue {
-    constructor(time, cur) {
-        this.heap = [{ time, cur }];
-    }
-    enqueue(time, cur) {
-        this.heap.push({ time, cur });
-        this.heapifyUp(this.heap.length - 1);
-    }
-    dequeue() {
-        const min = this.heap[0];
-        const temp = this.heap.pop();
-        if (this.heap.length > 0) {
-            this.heap[0] = temp;
-            this.heapifyDown(0);
-        }
-        return min;
-    }
-    heapifyUp(idx) {
-        while (idx > 0) {
-            let pIdx = (idx - 1) >> 1;
-            if (this.heap[idx].time < this.heap[pIdx].time) {
-                [this.heap[idx], this.heap[pIdx]] = [this.heap[pIdx], this.heap[idx]];
-            } else break;
-            idx = pIdx;
-        }
-    }
-    heapifyDown(idx) {
-        while (idx < this.heap.length - 1) {
-            let lcIdx = (idx << 1) + 1;
-            let rcIdx = (idx << 1) + 2;
-            let smaller = idx;
-            if (this.heap[lcIdx] && this.heap[lcIdx].time < this.heap[smaller].time) smaller = lcIdx;
-            if (this.heap[rcIdx] && this.heap[rcIdx].time < this.heap[smaller].time) smaller = rcIdx;
-            if (smaller === idx) break;
-            [this.heap[smaller], this.heap[idx]] = [this.heap[idx], this.heap[smaller]];
-            idx = smaller;
-        }
-    }
-    size() {
-        return this.heap.length;
-    }
-}
+const queue = [[0, N]];
+visited[N] = true;
+double(0, N * 2);
+let front = 0;
+let answer;
 
-const pq = new PriorityQueue(0, N);
-distance[N] = 0;
-
-while (pq.size() > 0) {
-    const { time, cur } = pq.dequeue();
-    for (const next of [cur - 1, cur + 1, cur * 2]) {
-        let cost;
-        if (next === cur * 2) cost = 0;
-        else cost = 1;
-        if (distance[next] > distance[cur] + cost) {
-            distance[next] = distance[cur] + cost;
-            pq.enqueue(time + cost, next);
+while (queue.length > front) {
+    const [time, cur] = queue[front++];
+    if (cur === K) {
+        answer = time;
+        break;
+    }
+    for (const next of [cur - 1, cur + 1]) {
+        if (!visited[next] && next >= 0 && next <= 100000) {
+            queue.push([time + 1, next]);
+            visited[next] = true;
+            double(time + 1, next * 2);
         }
     }
 }
 
-console.log(distance[K]);
+function double(time, cur) {
+    if (cur > 100000 || visited[cur] || cur === 0) return;
+    double(time, cur * 2);
+    queue.push([time, cur]);
+    visited[cur] = true;
+}
+
+console.log(answer);
