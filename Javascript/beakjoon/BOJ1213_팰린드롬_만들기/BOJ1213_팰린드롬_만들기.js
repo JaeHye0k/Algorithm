f2141d2decb3c37a51c4b8710826b6e04065eea2
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-06-30 11:45:35 +0900

    구현

---
+++ Javascript/beakjoon/BOJ1213_팰린드롬_만들기/BOJ1213_팰린드롬_만들기.js
@@ -0,0 +1,34 @@
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('');
+input.sort(); // 알파벳 오름차순 정렬
+
+let left = 0;
+let right = input.length;
+let mid = (left + right) >> 1;
+
+const palindrome = Array(right);
+const alphabet = new Map();
+for (const c of input) {
+    if (alphabet.has(c)) alphabet.set(c, alphabet.get(c) + 1);
+    else alphabet.set(c, 1);
+}
+
+let oddCount = 0;
+for (let [key, value] of alphabet) {
+    if (value % 2 === 1) {
+        oddCount++;
+        palindrome[mid] = key;
+        value--;
+    }
+    // 개수가 홀수인 알파벳이 2개 이상일 경우
+    if (oddCount > 1) {
+        console.log("I'm Sorry Hansoo");
+        process.exit();
+    }
+    for (let i = 0; i < value / 2; i++) {
+        palindrome[left++] = key;
+        palindrome[right--] = key;
+    }
+}
+
+console.log(palindrome.join(''));
