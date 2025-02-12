bbd73e15071502d9f3e040685a27a60077a418c8
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-02-12 16:43:46 +0900

    [Gold III] Title: 택배, Time: 272 ms, Memory: 18348 KB -BaekjoonHub

---
+++ 백준/Gold/1719. 택배/택배.js
@@ -0,0 +1,32 @@
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+const [n, m] = input[0].split(' ').map(Number);
+const path = input.slice(1).map((e) => e.split(' ').map(Number));
+const distance = Array.from({ length: n + 1 }, () => Array(n + 1).fill(Infinity));
+let arr = Array.from({ length: n + 1 }, () => Array(n + 1));
+path.forEach(([s, e, cost]) => {
+    distance[s][e] = cost;
+    distance[e][s] = cost;
+    arr[s][e] = e;
+    arr[e][s] = s;
+});
+for (let k = 1; k <= n; k++) {
+    for (let a = 1; a <= n; a++) {
+        for (let b = 1; b <= n; b++) {
+            if (a === b) {
+                arr[a][b] = '-';
+                continue;
+            }
+            if (distance[a][b] > distance[a][k] + distance[k][b]) {
+                distance[a][b] = distance[a][k] + distance[k][b];
+                arr[a][b] = arr[a][k];
+            }
+        }
+    }
+}
+arr = arr.slice(1).map((e) => e.slice(1));
+let answer = '';
+arr.forEach((row) => {
+    answer += row.join(' ') + '\n';
+});
+console.log(answer.trimEnd());
