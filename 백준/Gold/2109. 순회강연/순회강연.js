3b0e3fc284ee89a15978b9f30bdb887b868f4bdf
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-02-12 16:45:56 +0900

    [Gold III] Title: 순회강연, Time: 276 ms, Memory: 15448 KB -BaekjoonHub

---
+++ 백준/Gold/2109. 순회강연/순회강연.js
@@ -0,0 +1,26 @@
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+const n = +input[0];
+const pd = [];
+let maxDay = 0;
+for (let i = 1; i <= n; i++) {
+    const [pay, day] = input[i].split(' ').map(Number);
+    pd.push([pay, day]);
+    maxDay = Math.max(maxDay, day);
+}
+// 날짜순, 강연료 내림차순 정렬
+pd.sort((a, b) => b[0] - a[0]);
+const days = Array(maxDay + 1).fill(0);
+for (let [pay, day] of pd) {
+    if (days[day] === 0) days[day] = pay;
+    else {
+        for (let i = day - 1; i >= 1; i--) {
+            if (days[i] === 0) {
+                days[i] = pay;
+                break;
+            }
+        }
+    }
+}
+
+console.log(days.reduce((acc, cur) => acc + cur, 0));
