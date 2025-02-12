ae7ec1daea43b7a88a28a00151dfce548fee94ec
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-02-12 16:47:28 +0900

    [Gold III] Title: 구간 나누기, Time: 120 ms, Memory: 9784 KB -BaekjoonHub

---
+++ 백준/Gold/2228. 구간 나누기/구간 나누기.js
@@ -0,0 +1,14 @@
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+const [N, M] = input[0].split(' ').map(Number);
+const arr = [0].concat(input.slice(1).map(Number));
+const dp1 = Array.from({ length: N + 1 }, () => [0].concat(Array(M).fill(-Infinity)));
+const dp2 = Array.from({ length: N + 1 }, () => [0].concat(Array(M).fill(-Infinity)));
+
+for (let i = 1; i <= N; i++) {
+    for (let j = 1; j <= Math.min(M,Math.ceil(i/2)); j++) {
+        dp1[i][j] = Math.max(dp1[i - 1][j], dp2[i - 1][j]);
+        dp2[i][j] = Math.max(dp1[i - 1][j - 1], dp2[i - 1][j]) + arr[i];
+    }
+}
+console.log(Math.max(dp1[N][M], dp2[N][M]));
