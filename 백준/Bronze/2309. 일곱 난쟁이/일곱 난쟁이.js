20954dac8024c1c8c7951003ab69ab4d91fb15d9
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-02-12 16:48:15 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 128 ms, Memory: 9464 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./Javascript/input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n").map(Number);
+const { stdout } = require("process");
+
+let total = input.reduce((acc, cur) => (acc += cur));
+let index = [];
+
+for (let i = 0; i < 9; i++) {
+    let temp = total - input[i];
+    for (let j = 0; j < 9; j++) {
+        if (j !== i && temp - input[j] === 100) {
+            index.push(i);
+            index.push(j);
+            break;
+        }
+    }
+}
+
+index = index.splice(0, 2);
+let answer = input.filter((e, i) => !index.includes(i));
+answer.sort((a, b) => a - b);
+for (let dwarf of answer) {
+    process.stdout.write(dwarf + "\n");
+}
