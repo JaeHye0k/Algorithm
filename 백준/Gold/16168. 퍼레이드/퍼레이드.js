c6876ca8a5cb52d9fd4ed36bd1c1ec556ca5c953
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-02-12 17:17:17 +0900

    [Gold IV] Title: 퍼레이드, Time: 184 ms, Memory: 11856 KB -BaekjoonHub

---
+++ 백준/Gold/16168. 퍼레이드/퍼레이드.js
@@ -0,0 +1,41 @@
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+const [v, e] = input[0].split(' ').map(Number);
+const arr = input.slice(1).map((e) => e.split(' ').map(Number));
+const degree = Array(v + 1).fill(0);
+const parent = Array.from({ length: v + 1 }, (_, i) => i);
+arr.forEach(([s, e]) => {
+    union(s, e);
+    degree[s]++;
+    degree[e]++;
+});
+
+// 오일러 회로이거나 오일러 경로인지 판별
+// 차수가 홀수인 정점이 2개가 넘으면 오일러 회로, 오일러 경로가 될 수 없음
+let odd = 0;
+for (let i = 1; i <= v; i++) {
+    if (degree[i] % 2 === 1) odd++;
+}
+
+let same = find(1);
+for (let i = 2; i <= v; i++) {
+    // 하나의 컴포넌트로 연결되어있지 않다면
+    if (find(i) !== same) {
+        console.log('NO');
+        process.exit();
+    }
+}
+
+if (odd === 2 || odd === 0) console.log('YES');
+else console.log('NO');
+
+function find(x) {
+    if (parent[x] !== x) parent[x] = find(parent[x]);
+    return parent[x];
+}
+
+function union(a, b) {
+    a = find(a);
+    b = find(b);
+    parent[b] = a;
+}
