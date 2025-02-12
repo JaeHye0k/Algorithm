d63e59c32c61dd62fd361fac63c370f912fd7666
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-02-12 17:22:08 +0900

    [Gold IV] Title: 민서의 응급 수술, Time: 360 ms, Memory: 41860 KB -BaekjoonHub

---
+++ 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.js
@@ -0,0 +1,33 @@
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+const [N, M] = input[0].split(' ').map(Number);
+const arr = input.slice(1, 1 + M).map((e) => e.split(' ').map(Number));
+const parent = Array.from({ length: N + 1 }, (_, i) => i);
+let cut = 0;
+let connect = 0;
+
+function unionFind() {
+    for (let i = 0; i < M; i++) {
+        const a = find(arr[i][0]);
+        const b = find(arr[i][1]);
+        // 연결할 경우 사이클이 발생한다면
+        if (a === b) cut++; // 연결 끊기 연산
+        else union(a, b);
+    }
+}
+
+function union(a, b) {
+    if (a < b) parent[b] = a;
+    else parent[a] = b;
+}
+
+function find(x) {
+    if (parent[x] !== x) parent[x] = find(parent[x]);
+    return parent[x];
+}
+
+unionFind();
+for (let i = 1; i <= N; i++) {
+    if (parent[i] === i) connect++;
+}
+console.log(cut + connect - 1);
