f9e45045e3207e622f3515aba439562a3049ab29
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-02-22 15:54:05 +0900

    [level 2] Title: 숫자의 표현, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,10 +1,10 @@
 function solution(n) {
     let answer = 0;
 
-    for (var i = 1; i <= Math.sqrt(n); i++) {
-        if (n % i === 0) { // i가 n의 약수이고,
-            if (i % 2 === 1) answer++; // 약수 i가 홀수라면 카운트 증가
-            if ((n / i) % 2 === 1 && i * i !== n) answer++; // n / i(다른 약수 쌍)도 홀수이면 카운트 증가(두 약수쌍이 동일할 때는 예외 ex. n=25 이고, i=5일때)
+    for (let k = 1; k <= Math.sqrt(n); k++) {
+        if (n % k === 0) { // i가 n의 약수이고,
+            if (k % 2 === 1) answer++; // 약수 i가 홀수라면 카운트 증가
+            if ((n / k) % 2 === 1 && k !== Math.sqrt(n)) answer++; // n / i(다른 약수 쌍)도 홀수이면 카운트 증가(두 약수쌍이 동일할 때는 예외 ex. n=25 이고, i=5일때)
         }
     }
 
