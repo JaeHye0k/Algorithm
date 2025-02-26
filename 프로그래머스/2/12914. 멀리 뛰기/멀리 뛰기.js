edfaa70925595dda8603a2c985015e92bec621da
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-02-26 17:54:03 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.37 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,17 @@
+function solution(n) {
+    const dp = new Array(n+1).fill(0);
+    dp[0] = dp[1] = 1;
+    if(n === 1) return 1;
+    
+    for(let i = 2; i <= n; i++) {
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+    }
+    
+    return dp[n];
+}
+
+/*
+    1칸 또는 2칸씩 이동할 수 있을 때, n칸까지 가기 위한 모든 경우의 수
+    
+    arr[n] = arr[n-1] + arr[n-2];
+*/
