96d13a7897555ea0c2ce812210130ba866f57d01
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-02-27 13:22:45 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+    return arr.reduce((acc, cur) => lcm(acc, cur));
+}
+
+function gcd(a, b) {
+    return b === 0 ? a : gcd(b, a % b); 
+}
+
+function lcm(a, b) {
+    return (a * b) / gcd(a, b);
+}
