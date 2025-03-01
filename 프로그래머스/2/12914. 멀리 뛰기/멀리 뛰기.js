68a00457a793ee6cf3e5a0173006c61c40ccbf71
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-03-01 10:25:26 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,7 +1,6 @@
 function solution(n) {
     const dp = new Array(n+1).fill(0);
     dp[0] = dp[1] = 1;
-    if(n === 1) return 1;
     
     for(let i = 2; i <= n; i++) {
         dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
