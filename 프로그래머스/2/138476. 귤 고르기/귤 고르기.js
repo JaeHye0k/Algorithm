940c777cfd9a5a819636ebf5bd30996dda7c96e3
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-03-06 20:23:45 +0900

    [level 2] Title: 귤 고르기, Time: 134.98 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,30 @@
+function solution(k, tangerine) {
+    const count = {};
+    let answer = 0;
+    
+    for(let i = 0; i < tangerine.length; i++) {
+        const species = tangerine[i];
+        if(count[species]) {
+            count[species]++;
+        } else {
+            count[species] = 1;
+        }
+    }
+    
+    const sortedArr = Object.entries(count).sort((a , b) => b[1] - a[1]);
+    
+    for(const [species, count] of sortedArr) {
+        answer++;
+        k -= count;
+        if(k <= 0) break;
+        
+    }
+    
+    return answer;
+}
+
+/*
+    1. 종류별로 개수 파악
+    2. 개수를 기준으로 내림차순 정렬
+    3. 앞에서부터 카운트 까내려가기
+*/
