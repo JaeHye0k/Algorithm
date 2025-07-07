1905c802bef8918c55c33ad5a5186cc3714c8692
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-07-07 11:10:50 +0900

    [level 2] Title: [1차] 캐시, Time: 32.87 ms, Memory: 44.5 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,41 +1,27 @@
 function solution(cacheSize, cities) {
-    cities = cities.map((city) => city.toLowerCase());
-    let cache = [];
+    const cache = [];
     let answer = 0;
+    
+    cities = cities.map((city) => city.toLowerCase());
+    
     for(let i=0; i<cities.length; i++) {
-        const idx = checkCache(cache, cities[i]);
+        const idx = cache.indexOf(cities[i]);
         if(idx === -1) {
             answer += 5;
-            cache.push(cities[i]);
-            LRU(cache, cacheSize);
         } else{
             answer += 1;
             cache.splice(idx, 1);
-            cache.push(cities[i]);
         }
+        cache.push(cities[i]);
+        if(cache.length > cacheSize) cache.shift();
         
     }
     return answer;
 }
 
-// cache hit, miss 판단
-function checkCache(cache, data) {
-    for(let i=0; i<cache.length; i++) {
-        if(data === cache[i]) return i; // Hit!
-    }
-    return -1; // Miss
-}
-
-function LRU(cache, size) {
-    if(cache.length > size) {
-        cache.shift();
-    }
-    return cache;
-}
 
 /*
 - 캐시에 없다면 실행시간+5, 캐시에 push
 - 캐시에 있다면 실행시간+1, 캐시에 push
 - 캐시의 마지막 인덱스가 가장 최신에 사용한 데이터
-- cache hit 일 경우 
 */
