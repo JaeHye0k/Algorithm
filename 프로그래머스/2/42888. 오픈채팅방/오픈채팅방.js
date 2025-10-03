f4af1c314f49506898da812bf3e4c670e2edf6c2
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-10-03 11:52:57 +0900

    [level 2] Title: 오픈채팅방, Time: 302.00 ms, Memory: 118 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -0,0 +1,61 @@
+function solution(record) {
+    const messages = [];
+    const users = new Map();
+    const userMessages = new Map();
+    
+        
+    function toMessage(message) {
+        messages.push(message);
+        return messages.length - 1;
+    }
+    
+    const actionMap = new Map([
+        ['Enter', (action, userId, nickname) => {
+            const idx = toMessage(`${userId}님이 들어왔습니다.`);
+            userMessages.set(userId, [...userMessages.get(userId) ?? [], idx]);
+            users.set(userId, nickname);
+        }],
+        ['Leave', (action, userId) => {
+            const idx = toMessage(`${userId}님이 나갔습니다.`);
+            userMessages.set(userId, [...userMessages.get(userId), idx]);
+        }],
+        ['Change', (action, userId, nickname) => users.set(userId, nickname)]
+    ]);
+
+    
+    for(const rc of record) {
+        const [action, userId, nickname] = rc.split(' ');
+        actionMap.get(action)(action, userId, nickname);
+    }
+    
+    for(const [userId, indices] of userMessages) {
+        const nickname = users.get(userId);
+        for(const i of indices) {
+            messages[i] = messages[i].replace(userId, nickname);
+        }
+    }
+    
+    return messages;
+}
+
+
+
+/*
+    users = Map{ [userId]: nickname } // 유저id 와 대응하는 닉네임 저장
+    messages = []; // 메시지가 저장될 배열 (닉네임 대신 userId가 표시됨)
+    
+    actionMap = Map{ [action]: message } // 행동에 대응하는 함수 저장
+        Enter: (action, userId) => messages.push("${userId} 님이 들어왔습니다.");
+        Leave: (action, userId) => messages.push("${userId} 님이 나갔습니다.");
+        Change: (userId, nickname) => users.set(user.get(userId), nickname);
+    
+    
+    // record를 action, userId, nickname으로 분리하는 함수
+    
+    // messages에 있는 userId를 nickname으로 변환하는 함수 (마지막에 실행)
+    
+    0. actionMap 정의
+    1. record를 순회
+    2. users 세팅, messages 추가
+    3. 
+*/
