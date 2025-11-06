5ca2456d5b29675ff77f08da51634c39891321a4
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-11-06 23:33:08 +0900

    [level 1] Title: 하샤드 수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,6 +1,11 @@
 function solution(x) {
-    const sum = x.toString().split('').map(Number).reduce((acc, cur) => acc + cur, 0);
-    return x % sum === 0;
+    let sum = 0;
+    let temp = x;
+    while(temp > 0) {
+        sum += temp % 10;
+        temp = Math.floor(temp / 10);
+    }
+    return x % sum === 0; 
 }
 
 /*
