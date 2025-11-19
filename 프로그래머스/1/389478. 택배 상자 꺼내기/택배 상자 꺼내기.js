3ab35d762afcb1243e447d9eda0378baccfd15d5
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-11-20 00:13:51 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
@@ -1,16 +1,15 @@
 function solution(n, w, num) {
     var answer = 0;
-    const arr = Array.from({length: Math.ceil(n / w)}, (_, i) => new Array(w).fill(0));
+    const {x: nx, y: ny} = getXY(n, w);
+    const {x: numX, y: numY} = getXY(num, w);
+    answer += ny - numY;
     
-    for(let i=1; i<=n; i++) {
-        const {x, y} = getXY(i, w);
-        arr[y][x] = i;
-    }
+    if(n % w === 0) return answer+1;
     
-    const {x, y} = getXY(num, w);
-    
-    for(let i=y; i<arr.length; i++) {
-        if(arr[i][x]) answer++;
+    if(ny % 2 === 0) {
+        if(numX <= (n - 1) % w) return answer+1;
+    } else {
+        if(w - numX - 1 <= (n - 1) % w) return answer+1;
     }
     
     return answer;
@@ -33,7 +32,16 @@ function getXY(num, w) {
         1. i번 상자가 짝수 행일 때 = (i - 1) % w (0번째 열부터 시작)
         2. i번 상자가 홀수 행일 때 = (w - (i - 1) % w) - 1 
     
-    2차원 배열에 상자를 순서에 맞게 쌓고
-    num상자의 위치를 파악하고,
-    행을 축으로 움직이면서 상자가 있는지 파악
+    num이 있는 행의 번호를 얻어
+    n의 행의 번호를 얻어 
+    두 행 번호의 차이만큼의 개수 차이 구하기(마지막 행이 포함됨)
+    마지막 행에 상자가 없다면 상자를 하나 빼줘야 함
+    n이 w로 나누어떨어지면 마지막 행까지 꽉찬것
+    나누어 떨어지지 않으면 나머지 개수만큼의 상자가 마지막 행에 있는것
+    마지막 행 번호가 짝수(또는 0)면 왼쪽에서 오른쪽, 홀수면 오른쪽에서 왼쪽으로 상자가 쌓임
+    n을 w로 나누었을 때 나머지를 구함
+    나머지가 3이라면 마지막 행에 상자가 3개 있는 것. 마지막 행 번호가 짝수라면 1, 2, 3, 홀수라면 3, 2, 1
+    num의 열 번호 매핑
+    num의 열 번호를 얻어
+    
 */
